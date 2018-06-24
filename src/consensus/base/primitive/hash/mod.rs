df77717d8cea4ba48795fa8c5d6a25e5bab696a6
Pascal Berrang <mail@paberr.net>
2018-06-24 16:57:06 -0600

    Implement hash functions

--- src/consensus/base/primitive/hash/mod.rs
+++ src/consensus/base/primitive/hash/mod.rs
@@ -1,14 +1,21 @@
 extern crate hex;
+extern crate blake2_rfc;
+extern crate argon2rs;
+extern crate sha2;
 
 use std::fmt;
 use std::str;
 use self::hex::{FromHex,FromHexError};
+use self::blake2_rfc::blake2b::Blake2b;
+use self::argon2rs::Argon2;
+use self::sha2::{Sha256,Digest};
 
 pub trait Hasher {
     type Output;
 
-    fn finish(&self) -> Self::Output;
-    fn write(&mut self, bytes: &[u8]);
+    fn finish(self) -> Self::Output;
+    fn write(&mut self, bytes: &[u8]) -> &mut Self;
+    fn digest(self, bytes: &[u8]) -> Self::Output;
 }
 
 pub trait Hash {
@@ -74,48 +81,113 @@ macro_rules! implement_hash {
 	}
 }
 
-implement_hash!(Blake2bHash, 32);
-implement_hash!(Argon2dHash, 32);
-implement_hash!(Sha256Hash, 32);
+const BLAKE2B_LENGTH : usize = 32;
+implement_hash!(Blake2bHash, BLAKE2B_LENGTH);
+pub struct Blake2bHasher(Blake2b);
 
-struct Blake2bHasher;
+impl Blake2bHasher {
+    pub fn new() -> Self {
+        return Blake2bHasher(Blake2b::new(BLAKE2B_LENGTH));
+    }
+}
+
+impl Default for Blake2bHasher {
+    fn default() -> Self {
+        return Blake2bHasher::new();
+    }
+}
 
 impl Hasher for Blake2bHasher {
     type Output = Blake2bHash;
 
-    fn finish(&self) -> Blake2bHash {
-        unimplemented!()
+    fn finish(self) -> Blake2bHash {
+        let result = self.0.finalize();
+        return Blake2bHash::from(result.as_bytes());
     }
 
-    fn write(&mut self, bytes: &[u8]) {
-        unimplemented!()
+    fn write(&mut self, bytes: &[u8]) -> &mut Blake2bHasher {
+        self.0.update(bytes);
+        return self;
+    }
+
+    fn digest(mut self, bytes: &[u8]) -> Blake2bHash {
+        self.write(bytes);
+        return self.finish();
     }
 }
 
-struct Argon2dHasher;
+const ARGON2D_LENGTH : usize = 32;
+const ARGON2_SALT : &'static str = "nimiqrocks!";
+const DEFAULT_ARGON2_COST : u32 = 512;
+implement_hash!(Argon2dHash, ARGON2D_LENGTH);
+pub struct Argon2dHasher(Argon2, Vec<u8>);
+
+impl Argon2dHasher {
+    pub fn new(passes: u32, lanes: u32, kib: u32) -> Self {
+        return Argon2dHasher(Argon2::new(passes, lanes, kib, argon2rs::Variant::Argon2d).unwrap(), vec![]);
+    }
+
+    fn hash(&self, bytes: &[u8]) -> Argon2dHash {
+        let mut out = [0u8; ARGON2D_LENGTH];
+        self.0.hash(&mut out, bytes, ARGON2_SALT.as_bytes(), &[0u8; 0], &[0u8; 0]);
+        return Argon2dHash::from(out);
+    }
+}
+
+impl Default for Argon2dHasher {
+    fn default() -> Self {
+        return Argon2dHasher::new(1, 1, DEFAULT_ARGON2_COST);
+    }
+}
 
 impl Hasher for Argon2dHasher {
     type Output = Argon2dHash;
 
-    fn finish(&self) -> Argon2dHash {
-        unimplemented!()
+    fn finish(self) -> Argon2dHash {
+        return self.hash(self.1.as_slice());
     }
 
-    fn write(&mut self, bytes: &[u8]) {
-        unimplemented!()
+    fn write(&mut self, bytes: &[u8]) -> &mut Argon2dHasher {
+        self.1.extend(bytes);
+        return self;
+    }
+
+    fn digest(self, bytes: &[u8]) -> Argon2dHash {
+        return self.hash(bytes);
     }
 }
 
-struct Sha256Hasher;
+const SHA256_LENGTH : usize = 32;
+implement_hash!(Sha256Hash, SHA256_LENGTH);
+pub struct Sha256Hasher(Sha256);
+
+impl Sha256Hasher {
+    pub fn new() -> Self {
+        return Sha256Hasher(Sha256::default());
+    }
+}
+
+impl Default for Sha256Hasher {
+    fn default() -> Self {
+        return Sha256Hasher::new();
+    }
+}
 
 impl Hasher for Sha256Hasher {
     type Output = Sha256Hash;
 
-    fn finish(&self) -> Sha256Hash {
-        unimplemented!()
+    fn finish(self) -> Sha256Hash {
+        let result = self.0.result();
+        return Sha256Hash::from(result.as_slice());
     }
 
-    fn write(&mut self, bytes: &[u8]) {
-        unimplemented!()
+    fn write(&mut self, bytes: &[u8]) -> &mut Sha256Hasher {
+        self.0.input(bytes);
+        return self;
+    }
+
+    fn digest(mut self, bytes: &[u8]) -> Sha256Hash {
+        self.write(bytes);
+        return self.finish();
     }
 }
