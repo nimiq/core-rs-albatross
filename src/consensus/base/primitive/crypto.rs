b1cbe36745909e1aa7ed03153518d4522dd36e38
Pascal Berrang <mail@paberr.net>
2018-06-25 09:33:36 -0600

    Implement CommitmentPair

--- src/consensus/base/primitive/crypto.rs
+++ src/consensus/base/primitive/crypto.rs
@@ -1,13 +1,15 @@
 extern crate ed25519_dalek;
+extern crate curve25519_dalek;
 extern crate rand;
 extern crate sha2;
 
-use self::rand::OsRng;
+use self::rand::{OsRng, Rng};
+use self::sha2::Digest;
+use self::curve25519_dalek::scalar::Scalar;
+use self::curve25519_dalek::edwards::EdwardsPoint;
 
 create_typed_array!(PublicKey, u8, 32);
 create_typed_array!(PrivateKey, u8, 32);
-create_typed_array!(Commitment, u8, 32);
-create_typed_array!(RandomSecret, u8, 32);
 
 impl PublicKey {
     pub fn sum(public_keys: Vec<PublicKey>) -> Self {
@@ -72,7 +74,41 @@ pub fn signature_verify(signature: Signature, public_key: PublicKey, data: &[u8]
 }
 
 #[derive(PartialEq,Eq)]
-pub struct CommitmentPair<'a> {
-    random_secret: &'a RandomSecret,
-    commitment: &'a Commitment
+pub struct RandomSecret(Scalar);
+#[derive(PartialEq,Eq)]
+pub struct Commitment(EdwardsPoint);
+
+#[derive(PartialEq,Eq)]
+pub struct CommitmentPair {
+    random_secret: RandomSecret,
+    commitment: Commitment
+}
+
+impl CommitmentPair {
+    pub fn generate() -> Option<CommitmentPair> {
+        // Create random 32 bytes.
+        let mut cspring: OsRng = OsRng::new().unwrap();
+        let mut randomness: [u8; 32] = [0u8; 32];
+        cspring.fill_bytes(&mut randomness);
+
+        // Decompress the 32 byte cryptographically secure random data to 64 byte.
+        let mut h: sha2::Sha512 = sha2::Sha512::default();
+        let mut hash:  [u8; 64] = [0u8; 64];
+
+        h.input(&randomness);
+        hash.copy_from_slice(h.result().as_slice());
+
+        // Reduce to valid scalar.
+        let scalar = Scalar::from_bytes_mod_order_wide(&hash);
+        if scalar == Scalar::zero() || scalar == Scalar::one() {
+            return None;
+        }
+
+        // Compute the point [secret]B. Let the string R be the encoding of this point.
+        let commitment = scalar * curve25519_dalek::constants::ED25519_BASEPOINT_POINT;
+
+        let rs = RandomSecret(scalar);
+        let ct = Commitment(commitment);
+        return Some(CommitmentPair { random_secret: rs, commitment: ct });
+    }
 }
