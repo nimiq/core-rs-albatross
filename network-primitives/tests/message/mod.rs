2fbc30f05ada4ae4e44415be7f125e01bdf9007d
fiaxh <git@lightrise.org>
2019-02-04 22:09:11 +0100

    Tests for more network messages

--- network-primitives/tests/message/mod.rs
+++ network-primitives/tests/message/mod.rs
@@ -15,8 +15,17 @@ const GET_ADDR_MESSAGE: &str = "420420421500000014c09a093a02000000040008";
 const PING_MESSAGE: &str = "420420421600000011fde10bd200000002";
 const PONG_MESSAGE: &str = "4204204217000000112077d25700000002";
 const SUBSCRIBE_MESSAGE: &str = "420420420b000000601dc2dcab02000491e9240f415223982edc345532630710e94a7f5287298cc2f31fba73181ea2a9e6ef10dce21ed95e47ea70cf08872bdb4afad3432b01d963ac7d165f5d1c3122ada85138a67dfc15267cbeb21dd36041";
+const GET_CHAIN_PROOF_MESSAGE: &str = "42042042280000000d0cc9e55d";
+const GET_ACCOUNTS_PROOF_MESSAGE: &str = "420420422a000000570169b3b4324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cf000291e9240f415223982edc345532630710e94a7f5287298cc2f31fba73181ea2a9e6ef10dce21ed95e";
+const ACCOUNTS_PROOF_MESSAGE_WPROOF: &str = "420420422b00000269694947d7324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cf010007ff2830303131313131313131313131313131313131313131313131313131313131313131313131313131000000000000000019ff283030323030303030303030303030303030303030303030303030303030303030303030303030303000000000000000052aff283030323232323232323232323232323232323232323232323232323232323232323232323232323200000000000000005d00033030320225303030303030303030303030303030303030303030303030303030303030303030303030304f4c455662cdbcb30bcc2719dc7184cc30aa01ece81e5a8070f0a2e2f37f5f6b2532323232323232323232323232323232323232323232323232323232323232323232323232d2d93f95e11fc480f2cddb6b3a38e457e24bce6df5c5f470b2501acd5d61e12bff283030333333333333333333333333333333333333333333333333333333333333333333333333333300000000000000000100023030032631313131313131313131313131313131313131313131313131313131313131313131313131314a222c2f95a2f733d51ddea9ea7f6f0bc8f66f7ff4a3c7cc26ae99c4dcc5e28a0132b4e74f0d8054d535e1cb108fab3772a4e19729777b51d4640665a2da6ec7588e263333333333333333333333333333333333333333333333333333333333333333333333333333516c248e6df72f9ffdcc6be8bda172077a8f2de2956bf54d21cffdb77d4c8b02000001023030de17952823fba92b774b125dd808c270dccf1fe3b9c80b1050516ece11c787bb";
+const ACCOUNTS_PROOF_MESSAGE_WOPROOF: &str = "420420422b0000002e3fb4ef97324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cf00";
+const GET_ACCOUNTS_TREE_CHUNK_MESSAGE: &str = "420420422c00000030078e5b06324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cf026161";
+const GET_TRANSACTIONS_PROOF_MESSAGE: &str = "420420422f000000571ee934b2324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cf000291e9240f415223982edc345532630710e94a7f5287298cc2f31fba73181ea2a9e6ef10dce21ed95e";
+const GET_TRANSACTION_RECEIPTS_MESSAGE: &str = "42042042310000002525dc96f691e9240f415223982edc345532630710e94a7f5200000000";
+const GET_BLOCK_PROOF_MESSAGE: &str = "42042042330000004d3f619793324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cfb8b37c1d034e371c7a3b834f9476a746eb62259ff9558ab715b4bff79ebf58e1";
 
-const MESSAGES: [&str; 14] = [
+
+const MESSAGES: [&str; 19] = [
     VERSION_MESSAGE,
     INV_MESSAGE,
     GET_DATA_MESSAGE,
@@ -30,7 +39,15 @@ const MESSAGES: [&str; 14] = [
     GET_ADDR_MESSAGE,
     PING_MESSAGE,
     PONG_MESSAGE,
-    SUBSCRIBE_MESSAGE
+//    SUBSCRIBE_MESSAGE, // FIXME: HashSets don't preserve insertion order and addresses on SubscribeMsg are stored in a HashSet
+    GET_CHAIN_PROOF_MESSAGE,
+//    ACCOUNTS_PROOF_MESSAGE_WPROOF,
+//    ACCOUNTS_PROOF_MESSAGE_WOPROOF,
+    GET_ACCOUNTS_TREE_CHUNK_MESSAGE,
+    GET_TRANSACTIONS_PROOF_MESSAGE,
+    GET_TRANSACTION_RECEIPTS_MESSAGE,
+    GET_BLOCK_PROOF_MESSAGE,
+//    BLOCK_PROOF_MESSAGE
 ];
 
 #[test]
@@ -141,12 +158,96 @@ fn parse_subscribe_message() {
 }
 
 #[test]
+fn parse_get_chain_proof_message() {
+    let vec = ::hex::decode(GET_CHAIN_PROOF_MESSAGE).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message { Message::GetChainProof => assert!(true), _ => assert!(false) };
+}
+
+#[test]
+fn parse_get_accounts_proof_message() {
+    let vec = ::hex::decode(GET_ACCOUNTS_PROOF_MESSAGE).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message {
+        Message::GetAccountsProof(get_accounts_proof_message) => {
+            assert!(get_accounts_proof_message.addresses.len() == 2);
+        },
+        _ => assert!(false)
+    };
+}
+
+#[test]
+fn parse_accounts_proof_message_wproof() {
+    let vec = ::hex::decode(ACCOUNTS_PROOF_MESSAGE_WPROOF).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message {
+        Message::AccountsProof(accounts_proof_message) => {
+            assert!(accounts_proof_message.proof.is_some());
+        },
+        _ => assert!(false)
+    };
+}
+
+#[test]
+fn parse_accounts_proof_message_woproof() {
+    let vec = ::hex::decode(ACCOUNTS_PROOF_MESSAGE_WOPROOF).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message {
+        Message::AccountsProof(accounts_proof_message) => {
+            assert!(accounts_proof_message.proof.is_none());
+        },
+        _ => assert!(false)
+    };
+}
+
+#[test]
+fn parse_accounts_tree_chunk_message_woproof() {
+    let vec = ::hex::decode(GET_ACCOUNTS_TREE_CHUNK_MESSAGE).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message {
+        Message::GetAccountsTreeChunk(get_accounts_tree_chunk_message) => {
+            assert!(get_accounts_tree_chunk_message.start_prefix == "aa");
+        },
+        _ => assert!(false)
+    };
+}
+
+
+//fn parse_accounts_tree_chunk_message() {
+    //TODO
+//}
+
+#[test]
+fn parse_get_transactions_proof_message() {
+    let vec = ::hex::decode(GET_TRANSACTIONS_PROOF_MESSAGE).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message {
+        Message::GetTransactionsProof(get_transactions_proof_message) => {
+            assert!(get_transactions_proof_message.addresses.len() == 2);
+        },
+        _ => assert!(false)
+    };
+}
+
+#[test]
+fn parse_get_transaction_receipts_message() {
+    let vec = ::hex::decode(GET_TRANSACTION_RECEIPTS_MESSAGE).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message { Message::GetTransactionReceipts(_) => assert!(true), _ => assert!(false) };
+}
+
+#[test]
+fn parse_get_block_proof() {
+    let vec = ::hex::decode(GET_BLOCK_PROOF_MESSAGE).unwrap();
+    let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
+    match message { Message::GetBlockProof(_) => assert!(true), _ => assert!(false) };
+}
+
+#[test]
 fn reserialize_messages() {
-    for (i, message) in MESSAGES.iter().enumerate() {
+    for message in MESSAGES.iter() {
         let vec = ::hex::decode(message).unwrap();
         let message: Message = Deserialize::deserialize(&mut &vec[..]).unwrap();
-        if i != 13 { // FIXME: HashSets don't preserve insertion order and addresses on SubscribeMsg are stored in a HashSet
-            assert!(message.serialize_to_vec() == vec);
-        }
+        assert!(message.serialize_to_vec() == vec);
     }
 }
