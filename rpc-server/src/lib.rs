0d8225bd79076fb83b6599fb5e484f15f2323f04
Janosch Gräf <janosch.graef@cispa.saarland>
2019-02-12 16:25:46 -0600

    RPC server: Implemented `transaction_to_obj` and use  parameter `include_transactions`.

--- rpc-server/src/lib.rs
+++ rpc-server/src/lib.rs
@@ -16,10 +16,10 @@ use std::net::{SocketAddr, IpAddr};
 
 use futures::future::Future;
 use hyper::Server;
-use json::{Array, JsonValue, Null};
+use json::{Array, JsonValue, Null, };
 
 use beserial::Serialize;
-use consensus::consensus::Consensus;
+use consensus::consensus::{Consensus, ConsensusEvent};
 use hash::{Argon2dHash, Blake2bHash, Hash};
 use primitives::block::{Block, Difficulty};
 use primitives::transaction::Transaction;
@@ -30,21 +30,83 @@ pub mod error;
 
 pub struct JsonRpcHandler {
     consensus: Arc<Consensus>,
-    consensus_state: &'static str
+    consensus_state: &'static str,
+    starting_block: u32,
 }
 
 impl JsonRpcHandler {
     pub fn new(consensus: Arc<Consensus>) -> Self {
-        let res = JsonRpcHandler { 
-            consensus,
-            consensus_state: "syncing"
+        let mut res = JsonRpcHandler {
+            consensus: consensus.clone(),
+            consensus_state: "syncing",
+            starting_block: consensus.blockchain.height(),
         };
-        // TODO: Listen for consensus events
+
+        // Listen for consensus events
+        /*
+        TODO: We might need an Arc to the JsonRpcHandler here
+        consensus.notifier.write().register(|e: ConsensusEvent| {
+            match e {
+                Established => { res.consensus_state = "established" },
+                Lost => { res.consensus_state = "lost" },
+                Syncing => { res.consensus_state = "syncing" },
+                _ => ()
+            }
+        });
+        */
+
         res
     }
-    
+
+
+    fn peer_count(&self, _: Array) -> Result<JsonValue, JsonValue> {
+        Ok(self.consensus.network.peer_count().into())
+    }
+
+    fn consensus(&self, _: Array) -> Result<JsonValue, JsonValue> {
+        Ok(self.consensus_state.into())
+    }
+
+    fn syncing(&self, _: Array) -> Result<JsonValue, JsonValue> {
+        Ok(if self.consensus_state == "established" {
+            false.into()
+        }
+        else {
+            let current_block = self.consensus.blockchain.height();
+            object! {
+                "starting_block" => self.starting_block,
+                "current_block" => current_block,
+                "highest_block" => current_block // TODO
+            }
+        })
+    }
+
+    fn peer_list(&self, _params: Array) -> Result<JsonValue, JsonValue> {
+        Ok("TODO".into())
+    }
+
+    fn peer_state(&self, _params: Array) -> Result<JsonValue, JsonValue> {
+        Ok("TODO".into())
+    }
+
+    fn block_number(&self, _params: Array) -> Result<JsonValue, JsonValue> {
+        Ok(self.consensus.blockchain.height().into())
+    }
+
+    fn get_block_transaction_count_by_hash(&self, params: Array) -> Result<JsonValue, JsonValue> {
+        Ok("TODO".into())
+    }
+
+    fn get_block_transaction_count_by_number(&self, params: Array) -> Result<JsonValue, JsonValue> {
+        Ok("TODO".into())
+    }
+
+    fn get_block_by_hash(&self, params: Array) -> Result<JsonValue, JsonValue> {
+        self.block_to_obj(&self.block_by_hash(params.get(0).unwrap_or(&Null))?, params.get(1).and_then(|v| v.as_bool()).unwrap_or(false))
+    }
+
     fn get_block_by_number(&self, params: Array) -> Result<JsonValue, JsonValue> {
-        self.block_to_obj(&self.block_by_number(params.get(0).unwrap_or(&Null))?, true)
+        self.block_to_obj(&self.block_by_number(params.get(0).unwrap_or(&Null))?, params.get(1).and_then(|v| v.as_bool()).unwrap_or(false))
     }
     
     fn block_by_number(&self, number: &JsonValue) -> Result<Block, JsonValue> {
@@ -64,7 +126,18 @@ impl JsonRpcHandler {
         if block_number == 0 {
             block_number = 1;
         }
-        self.consensus.blockchain.get_block_at(block_number, true).ok_or_else(|| object!{"message" => "Block not found"})
+        self.consensus.blockchain
+            .get_block_at(block_number, true)
+            .ok_or_else(|| object!{"message" => "Block not found"})
+    }
+
+    fn block_by_hash(&self, hash: &JsonValue) -> Result<Block, JsonValue> {
+        let hash = hash.as_str()
+            .ok_or_else(|| object!{"message" => "Hash must be a string"})
+            .and_then(|s| Blake2bHash::from_str(s)
+                .map_err(|_| object!{"message" => "Invalid Blake2b hash"}))?;
+        self.consensus.blockchain.get_block(&hash, false, true)
+            .ok_or_else(|| object!{"message" => "Block not found"})
     }
     
     fn block_to_obj(&self, block: &Block, include_transactions: bool) -> Result<JsonValue, JsonValue> {
@@ -77,43 +150,63 @@ impl JsonRpcHandler {
             "bodyHash" => block.header.body_hash.to_hex(),
             "accountsHash" => block.header.accounts_hash.to_hex(),
             "miner" => block.body.as_ref().map(|body| body.miner.to_hex().into()).unwrap_or(Null),
-            //"minerAddress" // TODO
+            "minerAddress" => block.body.as_ref().map(|body| body.miner.to_user_friendly_address().into()).unwrap_or(Null),
             "difficulty" => Difficulty::from(block.header.n_bits).to_string(),
             "extraData" => block.body.as_ref().map(|body| hex::encode(&body.extra_data).into()).unwrap_or(Null),
             "size" => block.serialized_size(),
             "timestamp" => block.header.timestamp,
             "transactions" => JsonValue::Array(block.body.as_ref().map(|body| if include_transactions { 
-                body.transactions.iter().map(|tx| self.transaction_to_obj(tx, block, 0).unwrap_or(Null)).collect()
+                body.transactions.iter().enumerate().map(|(i, tx)| self.transaction_to_obj(tx, Some(block), i).unwrap_or(Null)).collect()
             } else { 
                 body.transactions.iter().map(|tx| tx.hash::<Blake2bHash>().to_hex().into()).collect()
             }).unwrap_or(vec![])),
         })
     }
     
-    fn transaction_to_obj(&self, transaction: &Transaction, block: &Block, i: usize) -> Result<JsonValue, JsonValue> {
-        Ok(Null)
-    }
-
-    fn block_number(&self, _params: Array) -> Result<JsonValue, JsonValue> {
-        Ok(self.consensus.blockchain.height().into())
-    }
-
-    fn peer_count(&self, _params: Array) -> Result<JsonValue, JsonValue> {
-        Ok(self.consensus.network.peer_count().into())
-    }
-
-    fn consensus(&self, _params: Array) -> Result<JsonValue, JsonValue> {
-        Ok(self.consensus_state.into())
+    fn transaction_to_obj(&self, transaction: &Transaction, block: Option<&Block>, i: usize) -> Result<JsonValue, JsonValue> {
+        let header = block.as_ref().map(|b| &b.header);
+        Ok(object!{
+            "hash" => transaction.hash::<Blake2bHash>().to_hex(),
+            "blockHash" => header.map(|h| h.hash::<Blake2bHash>().to_hex().into()).unwrap_or(Null),
+            "blockNumber" => header.map(|h| h.height.into()).unwrap_or(Null),
+            "blockHash" => header.map(|h| h.timestamp.into()).unwrap_or(Null),
+            "confirmations" => header.map(|b| (self.consensus.blockchain.height() - b.height).into()).unwrap_or(Null),
+            "transactionIndex" => i,
+            "from" => transaction.sender.to_hex(),
+            "fromAddress" => transaction.sender.to_user_friendly_address(),
+            "to" => transaction.recipient.to_hex(),
+            "toAddress" => transaction.recipient.to_user_friendly_address(),
+            "value" => u64::from(transaction.value),
+            "fee" => u64::from(transaction.fee),
+            "data" => hex::encode(&transaction.data),
+            "flags" => transaction.flags.bits()
+        })
     }
 }
 
 impl jsonrpc::Handler for JsonRpcHandler {
     fn get_method(&self, name: &str) -> Option<fn(&Self, Array) -> Result<JsonValue, JsonValue>> {
+        // TODO: Apply method white-listing
         match name {
-            "getBlockByNumber" => Some(JsonRpcHandler::get_block_by_number),
-            "blockNumber" => Some(JsonRpcHandler::block_number),
+            // Network
             "peerCount" => Some(JsonRpcHandler::peer_count),
+            "syncing" => Some(JsonRpcHandler::syncing),
             "consensus" => Some(JsonRpcHandler::consensus),
+            "peerCount" => Some(JsonRpcHandler::peer_list),
+            "peerState" => Some(JsonRpcHandler::peer_count),
+
+            // Transactions
+            // TODO
+
+            // Blockchain
+            "blockNumber" => Some(JsonRpcHandler::block_number),
+            "getBlockTransactionCountByHash" => Some(JsonRpcHandler::get_block_transaction_count_by_hash),
+            "getBlockTransactionCountByNumber" => Some(JsonRpcHandler::get_block_transaction_count_by_number),
+            "getBlockByHash" => Some(JsonRpcHandler::get_block_by_hash),
+            "getBlockByNumber" => Some(JsonRpcHandler::get_block_by_number),
+
+
+
             _ => None
         }
     }
