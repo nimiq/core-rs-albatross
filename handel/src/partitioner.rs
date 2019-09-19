673b4920b2459f74a0c04e8acc371fb459b95f8e
Janosch Gräf <janosch@nimiq.com>
2019-09-19 12:49:28 +0200

    Fix num_levels for less than 2 validators

--- handel/src/partitioner.rs
+++ handel/src/partitioner.rs
@@ -49,10 +49,11 @@ pub struct BinomialPartitioner {
 
 impl BinomialPartitioner {
     pub fn new(node_id: usize, num_ids: usize) -> Self {
+        let num_levels = if num_ids > 1 { log2(num_ids - 1) + 2 } else { 1 };
         Self {
             node_id,
             num_ids,
-            num_levels: log2(num_ids - 1) + 2
+            num_levels,
         }
     }
 }
