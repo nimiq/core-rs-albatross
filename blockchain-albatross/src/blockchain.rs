6265cc56a44b90977cd8e2aed3b50083b28c110e
Janosch Gräf <janosch@nimiq.com>
2019-10-31 16:23:24 -0600

    Added some information to the panic

--- blockchain-albatross/src/blockchain.rs
+++ blockchain-albatross/src/blockchain.rs
@@ -1177,7 +1177,7 @@ impl<'env> Blockchain<'env> {
         } else {
             let macro_block = self.chain_store
                 .get_block_at(policy::macro_block_before(block_number), true, Some(&txn))
-                .expect("Failed to determine slots - preceding macro block not found")
+                .unwrap_or_else(|| panic!("Failed to determine slots - preceding macro block not found: block_number={}, view_number={}, state.block_number()={}", block_number, view_number, state.block_number()))
                 .unwrap_macro();
 
             // Get slots of epoch
