b96dfdd6c253576fc38165d4eebb130c25bf3133
Janosch Gräf <janosch@nimiq.com>
2019-11-09 15:02:10 -0600

    Change log level for slashing in staking contract to TRACE

--- primitives/account/src/staking_contract.rs
+++ primitives/account/src/staking_contract.rs
@@ -617,9 +617,8 @@ impl AccountInherentInteraction for StakingContract {
 
         let mut to_pay = inherent.value;
         let staker_address: Address = Deserialize::deserialize(&mut &inherent.data[..])?;
-        let staker_human_address = staker_address.to_user_friendly_address();
 
-        warn!("Slashing {} with {}", &staker_human_address, to_pay);
+        trace!("commit_inherent: slashing {} with {}", &staker_address.to_user_friendly_address(), to_pay);
 
         if let Some(active_stake) = self.active_stake_by_address.remove(&staker_address) {
             self.active_stake_sorted.remove(&active_stake);
