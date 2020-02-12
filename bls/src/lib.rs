3f93390abc5fdbe81f93e5a0c9e692bc0ba520c6
brunoffranca <bruno@franca.xyz>
2020-02-12 11:05:52 -0600

    Updated the hash_to_g1 to be consistent with the nano-sync.

--- bls/src/lib.rs
+++ bls/src/lib.rs
@@ -8,6 +8,7 @@ extern crate nimiq_utils as utils;
 
 // Imports the types needed for elliptic curve algebra
 use algebra::{
+    biginteger::BigInteger,
     bytes::{FromBytes, ToBytes},
     curves::{
         bls12_377::{Bls12_377, G1Affine, G1Projective, G2Affine, G2Projective},
@@ -17,18 +18,18 @@ use algebra::{
     rand::UniformRand,
     serialize::{CanonicalDeserialize, CanonicalSerialize, SerializationError},
 };
+use num_traits::{One, Zero};
 
-// Zero is for used for getting the point at infinity from a curve.
-use num_traits::Zero;
+// Used for the Blake2X hashing.
+use crypto_primitives::prf::Blake2sWithParameterBlock;
 
 // Used for the random number generation
-use rand::SeedableRng;
-use rand_chacha::ChaChaRng;
 use utils::key_rng::{CryptoRng, Rng};
 pub use utils::key_rng::{SecureGenerate, SecureRng};
 
 #[cfg(feature = "beserial")]
 use beserial::{Deserialize, Serialize};
+use blake2_rfc::blake2s::Blake2s;
 use failure::Fail;
 use hash::{Blake2sHash, Hash};
 use hex::FromHexError;
@@ -48,3 +49,26 @@ pub use types::*;
 
 // Specifies the hash algorithm used for signatures
 pub type SigHash = Blake2sHash;
+
+/// If bytes is a little endian representation of a number, this would return the bits of the
+/// number in descending order
+pub fn bytes_to_bits(bytes: &Vec<u8>, bits_to_take: usize) -> Vec<bool> {
+    let mut bits = vec![];
+    for i in 0..bytes.len() {
+        let mut byte = bytes[i];
+        for _ in 0..8 {
+            bits.push((byte & 1) == 1);
+            byte >>= 1;
+        }
+    }
+
+    let bits_filtered = bits
+        .into_iter()
+        .take(bits_to_take)
+        .collect::<Vec<bool>>()
+        .into_iter()
+        .rev()
+        .collect();
+
+    bits_filtered
+}
--- bls/src/types/signature.rs
+++ bls/src/types/signature.rs
@@ -11,16 +11,48 @@ impl Signature {
     /// Maps an hash to a elliptic curve point in the G1 group. It is required to create signatures.
     /// It is also known as "hash-to-curve".
     pub(crate) fn hash_to_g1(h: SigHash) -> G1Projective {
-        let rng = &mut ChaChaRng::from_seed(h.into());
+        // This extends the input hash from 32 bytes to 48 bytes using the Blake2X algorithm.
+        // See https://blake2.net/blake2x.pdf for more details.
+        let mut bytes = vec![];
+        let digest_length = vec![32, 16];
+        for i in 0..2 {
+            let blake2x = Blake2sWithParameterBlock {
+                digest_length: digest_length[i],
+                key_length: 0,
+                fan_out: 0,
+                depth: 0,
+                leaf_length: 32,
+                node_offset: i as u32,
+                xof_digest_length: 48,
+                node_depth: 0,
+                inner_length: 32,
+                salt: [0; 8],
+                personalization: [0; 8],
+            };
+            let mut state = Blake2s::with_parameter_block(&blake2x.parameters());
+            state.update(h.as_bytes());
+            let mut result = state.finalize().as_bytes().to_vec();
+            bytes.append(&mut result);
+        }
+
+        // This converts the hash output into a x-coordinate and a y-coordinate for an elliptic curve point. At this time, it is not guaranteed to be a valid point.
+        let bits = bytes_to_bits(&bytes, 378);
+        let mut x_coordinate = Fq::new(BigInteger::from_bits(&bits[..377]));
+        let y_coordinate = bits[377];
+        // bytes[47] &= 0b00000011;
+        // let mut x_coordinate_too = Fq::new(FromBytes::read(&bytes[..]).unwrap());
+        // assert_eq!(x_coordinate, x_coordinate_too);
+
+        // This implements the try-and-increment method of converting an integer to an elliptic curve point.
+        // See https://eprint.iacr.org/2009/226.pdf for more details.
         loop {
-            let x_coordinate = Fq::rand(rng);
-            let y_coordinate = bool::rand(rng);
             let point = G1Affine::get_point_from_x(x_coordinate, y_coordinate);
             if point.is_some() {
                 let point = G1Affine::from(point.unwrap());
                 let g1 = point.scale_by_cofactor();
                 return g1;
             }
+            x_coordinate += &Fq::one();
         }
     }
 
