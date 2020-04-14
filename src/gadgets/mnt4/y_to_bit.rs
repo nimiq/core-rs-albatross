2578968035c523858e8ce641e48a58ee83f565b8
Pascal Berrang <contact@paberr.net>
2020-04-14 18:01:19 +0200

    Fix parts of the YToBitsGadget for Fp3. It still misses some constraints as described here: https://github.com/celo-org/bls-zexe/issues/147

--- src/gadgets/mnt4/y_to_bit.rs
+++ src/gadgets/mnt4/y_to_bit.rs
@@ -3,8 +3,10 @@ use std::ops::Neg;
 use algebra::{mnt4_753::Fr as MNT4Fr, mnt6_753::Fq, One, PrimeField};
 use r1cs_core::SynthesisError;
 use r1cs_std::bits::boolean::Boolean;
+use r1cs_std::eq::{ConditionalEqGadget, ConditionalOrEqualsGadget, EqGadget};
 use r1cs_std::mnt6_753::{FqGadget, G1Gadget, G2Gadget};
 use r1cs_std::prelude::{AllocGadget, FieldGadget};
+use r1cs_std::select::CondSelectGadget;
 use r1cs_std::{Assignment, ToBitsGadget};
 
 /// A gadget that takes an elliptic curve point as input and outputs a single bit representing the
@@ -60,6 +62,14 @@ impl YToBitGadget {
         pk: &G2Gadget,
     ) -> Result<Boolean, SynthesisError> {
         let half_plus_one_neg = (Fq::from_repr(Fq::modulus_minus_one_div_two()) + &Fq::one()).neg();
+        let y_c2_bit = Boolean::alloc(cs.ns(|| "alloc y c2 bit"), || {
+            if pk.y.c2.get_value().is_some() {
+                let half = Fq::modulus_minus_one_div_two();
+                Ok(pk.y.c2.get_value().get()?.into_repr() > half)
+            } else {
+                Err(SynthesisError::AssignmentMissing)
+            }
+        })?;
         let y_c1_bit = Boolean::alloc(cs.ns(|| "alloc y c1 bit"), || {
             if pk.y.c1.get_value().is_some() {
                 let half = Fq::modulus_minus_one_div_two();
@@ -76,10 +86,18 @@ impl YToBitGadget {
                 Err(SynthesisError::AssignmentMissing)
             }
         })?;
-        let y_eq_bit = Boolean::alloc(cs.ns(|| "alloc y eq bit"), || {
-            if pk.y.c0.get_value().is_some() {
+        let y_eq_bit_c2 = Boolean::alloc(cs.ns(|| "alloc y c2 eq bit"), || {
+            if pk.y.c2.get_value().is_some() {
                 let half = Fq::modulus_minus_one_div_two();
-                Ok(pk.y.c0.get_value().get()?.into_repr() == half)
+                Ok(pk.y.c2.get_value().get()?.into_repr() == half)
+            } else {
+                Err(SynthesisError::AssignmentMissing)
+            }
+        })?;
+        let y_eq_bit_c1 = Boolean::alloc(cs.ns(|| "alloc y c1 eq bit"), || {
+            if pk.y.c1.get_value().is_some() {
+                let half = Fq::modulus_minus_one_div_two();
+                Ok(pk.y.c1.get_value().get()?.into_repr() == half)
             } else {
                 Err(SynthesisError::AssignmentMissing)
             }
@@ -87,14 +105,32 @@ impl YToBitGadget {
         let y_bit = Boolean::alloc(cs.ns(|| "alloc y bit"), || {
             if pk.y.c1.get_value().is_some() {
                 let half = Fq::modulus_minus_one_div_two();
-                if pk.y.c1.get_value().get()?.into_repr() > half {
+                if pk.y.c2.get_value().get()?.into_repr() > half {
                     Ok(true)
-                } else if pk.y.c1.get_value().get()?.into_repr() == half
+                } else if pk.y.c2.get_value().get()?.into_repr() == half
+                    && pk.y.c1.get_value().get()?.into_repr() > half
+                {
+                    Ok(true)
+                } else if pk.y.c2.get_value().get()?.into_repr() == half
+                    && pk.y.c1.get_value().get()?.into_repr() == half
                     && pk.y.c0.get_value().get()?.into_repr() > half
                 {
                     Ok(true)
                 } else {
                     Ok(false)
+                }
+            } else {
+                Err(SynthesisError::AssignmentMissing)
+            }
+        })?;
+        let y_c2_adjusted = FqGadget::alloc(cs.ns(|| "alloc y c2"), || {
+            if pk.y.get_value().is_some() {
+                let half = Fq::modulus_minus_one_div_two();
+                let y_value = pk.y.c2.get_value().get()?;
+                if y_value.into_repr() > half {
+                    Ok(y_value - &(Fq::from_repr(half) + &Fq::one()))
+                } else {
+                    Ok(y_value)
                 }
             } else {
                 Err(SynthesisError::AssignmentMissing)
@@ -126,6 +162,24 @@ impl YToBitGadget {
                 Err(SynthesisError::AssignmentMissing)
             }
         })?;
+        let y_c2_bit_lc = y_c2_bit.lc(CS::one(), half_plus_one_neg);
+        // c2 + y_c2_bit * (-half + 1) = y_c2_adjusted
+        // if c2 < half
+        //   c2 + 0 = c2
+        // else
+        //   c2 - half + 1 = y_c2_adjusted
+        cs.enforce(
+            || "check y bit c2",
+            |lc| lc + (Fq::one(), CS::one()),
+            |lc| pk.y.c2.get_variable() + y_c2_bit_lc + lc,
+            |lc| y_c2_adjusted.get_variable() + lc,
+        );
+        let y_c2_adjusted_bits = &y_c2_adjusted.to_bits(cs.ns(|| "y c2 adjusted to bits"))?;
+        Boolean::enforce_smaller_or_equal_than::<_, _, Fq, _>(
+            cs.ns(|| "enforce y c2 smaller than modulus minus one div two"),
+            y_c2_adjusted_bits,
+            Fq::modulus_minus_one_div_two(),
+        )?;
         let y_c1_bit_lc = y_c1_bit.lc(CS::one(), half_plus_one_neg);
         cs.enforce(
             || "check y bit c1",
@@ -153,19 +207,22 @@ impl YToBitGadget {
             Fq::modulus_minus_one_div_two(),
         )?;
 
-        // (1-a)*(b*c) == o - a
-        // a is c1
-        // b is y_eq
-        // c is c0
+        // FIXME: y_eq_bit_{c1,c2} are still unconstrained: https://github.com/celo-org/bls-zexe/issues/147
 
-        let bc = Boolean::and(cs.ns(|| "and bc"), &y_eq_bit, &y_c0_bit)?;
-
-        cs.enforce(
-            || "enforce y bit derived correctly",
-            |lc| lc + (Fq::one(), CS::one()) + y_c1_bit.lc(CS::one(), Fq::one().neg()),
-            |_| bc.lc(CS::one(), Fq::one()),
-            |lc| lc + y_bit.lc(CS::one(), Fq::one()) + y_c1_bit.lc(CS::one(), Fq::one().neg()),
-        );
+        // o = if y_eq_bit_c2 { if y_eq_bit_c1 { y_c0_bit } else { y_c1_bit } } else { y_c2_bit }
+        let computed_inner_y_bit = Boolean::conditionally_select(
+            cs.ns(|| "select based on y_eq_bit_c1"),
+            &y_eq_bit_c1,
+            &y_c0_bit,
+            &y_c1_bit,
+        )?;
+        let computed_y_bit = Boolean::conditionally_select(
+            cs.ns(|| "select based on y_eq_bit_c2"),
+            &y_eq_bit_c2,
+            &computed_inner_y_bit,
+            &y_c2_bit,
+        )?;
+        y_bit.conditional_enforce_equal(cs.ns(|| "check y bit"), &computed_y_bit)?;
 
         Ok(y_bit)
     }
