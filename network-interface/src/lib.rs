a355124ac383e79e81ddf8cd1f07801b3c85892a
Pascal Berrang <contact@paberr.net>
2020-07-10 16:05:44 +0200

    Implement receive_from_all

--- network-interface/src/lib.rs
+++ network-interface/src/lib.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 use beserial::{Deserialize, ReadBytesExt, Serialize, SerializingError, WriteBytesExt};
+use futures::stream::{FusedStream, SelectAll};
+use futures::task::{Context, Poll};
 use futures::{
     channel::oneshot::{channel, Sender},
     future, stream, Stream, StreamExt, TryFutureExt,
@@ -11,7 +13,11 @@ use std::marker::PhantomData;
 use std::pin::Pin;
 use std::sync::{Arc, Weak};
 use std::time::Duration;
-use tokio::{task::spawn, time::timeout};
+use tokio::{
+    sync::broadcast::{Receiver as BroadcastReceiver, RecvError as BroadcastRecvError},
+    task::spawn,
+    time::timeout,
+};
 
 pub trait Message: Serialize + Deserialize + Send + Sync {
     const TYPE_ID: u64;
@@ -70,12 +76,27 @@ pub trait Peer: Send + Sync {
     async fn close(&self, ty: CloseReason);
 }
 
+pub enum NetworkEvent<P> {
+    PeerJoined(Arc<P>),
+    //PeerLeft(Arc<P>),
+}
+
+impl<P> Clone for NetworkEvent<P> {
+    fn clone(&self) -> Self {
+        match self {
+            NetworkEvent::PeerJoined(peer) => NetworkEvent::PeerJoined(Arc::clone(peer)),
+        }
+    }
+}
+
 #[async_trait]
 pub trait Network {
-    type PeerType: Peer;
+    type PeerType: Peer + 'static;
 
     fn get_peers(&self) -> &[Arc<Self::PeerType>];
     fn get_peer(&self, peer_id: PeerId) -> &Arc<Self::PeerType>;
+
+    fn subscribe_events(&self) -> BroadcastReceiver<NetworkEvent<Self::PeerType>>;
 
     async fn broadcast<T: Message>(&self, msg: &T) {
         future::join_all(self.get_peers().iter().map(|peer| {
@@ -86,13 +107,61 @@ pub trait Network {
         .await;
     }
 
-    //    // TODO: What if new peers join?
-    //    fn receive_from_all<'a, T: Message + 'a>(&self) -> Pin<Box<dyn Stream<Item = T> + Send + 'a>> {
-    //        select_all(self.get_peers().iter().map(|peer| peer.receive::<T>())).boxed()
-    //    }
+    fn receive_from_all<T: Message>(&self) -> ReceiveFromAll<T, Self::PeerType> {
+        ReceiveFromAll::new(self)
+    }
 }
 
 // .next() To get next item of stream.
+
+/// A wrapper around `SelectAll` that automatically subscribes to new peers.
+pub struct ReceiveFromAll<T: Message, P> {
+    inner: SelectAll<Pin<Box<dyn Stream<Item = T> + Send>>>,
+    event_stream:
+        Pin<Box<dyn FusedStream<Item = Result<NetworkEvent<P>, BroadcastRecvError>> + Send>>,
+}
+
+impl<T: Message, P: Peer + 'static> ReceiveFromAll<T, P> {
+    pub fn new<N: Network<PeerType = P> + ?Sized>(network: &N) -> Self {
+        ReceiveFromAll {
+            inner: stream::select_all(network.get_peers().iter().map(|peer| peer.receive::<T>())),
+            event_stream: Box::pin(network.subscribe_events().into_stream().fuse()),
+        }
+    }
+}
+
+impl<T: Message, P: Peer> Stream for ReceiveFromAll<T, P> {
+    type Item = T;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        loop {
+            match self.event_stream.poll_next_unpin(cx) {
+                Poll::Pending => break,
+                Poll::Ready(Some(Ok(NetworkEvent::PeerJoined(peer)))) => {
+                    // We have a new peer to receive from.
+                    self.inner.push(peer.receive::<T>())
+                }
+                #[allow(unreachable_patterns)]
+                Poll::Ready(Some(Ok(_))) => {} // Ignore others.
+                // The receiver lagged too far behind.
+                // Attempting to receive again will return the oldest message still retained by the channel.
+                // So, that's what we do.
+                Poll::Ready(Some(Err(BroadcastRecvError::Lagged(_)))) => {}
+                Poll::Ready(None) | Poll::Ready(Some(Err(BroadcastRecvError::Closed))) => {
+                    // There are no more active senders implying no further messages will ever be sent.
+                    return Poll::Ready(None); // Discard this stream entirely.
+                }
+            }
+        }
+        self.inner.poll_next_unpin(cx)
+    }
+}
+
+impl<T: Message, P: Peer> FusedStream for ReceiveFromAll<T, P> {
+    fn is_terminated(&self) -> bool {
+        self.inner.is_terminated() || self.event_stream.is_terminated()
+    }
+}
 
 struct RequestResponseState<Res: ResponseMessage> {
     current_request_identifier: u32,
