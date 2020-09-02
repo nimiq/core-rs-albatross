ef6dcd909dcd76728798dc50c54a7e4fb1051c23
brunoffranca <bruno@franca.xyz>
2020-09-02 02:50:04 +0100

    Deleted network crate.

--- network/src/address/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod peer_address_book;
-mod peer_address_seeder;
-pub mod peer_address_state;
--- network/src/connection/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod close_type;
-pub mod connection_info;
-pub mod connection_pool;
-pub mod network_agent;
-pub mod network_connection;
-mod signal_processor;
-
-pub use self::network_connection::*;
