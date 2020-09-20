2704b10ed948a349d4fe23e3c0ab648e37d895dd
styppo <mail@styp-rekowsky.de>
2020-09-20 18:56:03 +0200

    Port client crate to tokio 0.2

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,8 +5,8 @@ extern crate nimiq_lib as nimiq;
 use std::convert::TryFrom;
 use std::time::Duration;
 
-use futures::{future, Future, IntoFuture, Stream};
-use tokio::timer::Interval;
+use futures::{future, FutureExt, StreamExt, TryFutureExt};
+use tokio::runtime::Runtime;
 
 use nimiq::extras::deadlock::initialize_deadlock_detection;
 use nimiq::extras::logging::{initialize_logging, log_error_cause_chain};
@@ -25,128 +25,70 @@ fn main_inner() -> Result<(), Error> {
     let config_file = ConfigFile::find(Some(&command_line))?;
     trace!("Config file: {:#?}", config_file);
 
-    // Initialize logging with config values
+    // Initialize logging with config values.
     initialize_logging(Some(&command_line), Some(&config_file.log))?;
 
-    // Initialize panic hook
+    // Initialize panic hook.
     initialize_panic_reporting();
 
-    // Create config builder and apply command line and config file
-    // You usually want the command line to override config settings, so the order is important
+    // Create config builder and apply command line and config file.
+    // You usually want the command line to override config settings, so the order is important.
     let mut builder = ClientConfig::builder();
     builder.config_file(&config_file)?;
     builder.command_line(&command_line)?;
 
-    // finalize config
+    // Finalize config.
     let config = builder.build()?;
     debug!("Final configuration: {:#?}", config);
 
-    // We need to instantiate the client when the tokio runtime is already alive, so we use
-    // a lazy future for it.
-    tokio::run(
-        // TODO: Return this from `Client::into_future()`
-        future::lazy(move || {
-            // TODO: This is the initialization future
+    // We need to instantiate the client within the tokio runtime context, so we use a lazy future.
+    let init_future = future::lazy(move |_| {
+        // Create client from config.
+        info!("Initializing client");
+        let client: Client = Client::try_from(config)?;
+        client.initialize()?;
 
-            // Clone those now, because we pass ownership of config to Client
-            let protocol_config = config.protocol.clone();
-            let rpc_config = config.rpc_server.clone();
-            let metrics_config = config.metrics_server.clone();
-            let ws_rpc_config = config.ws_rpc_server.clone();
+        // TODO Initialize RpcServer and MetricsServer or do this in the Client.
 
-            // Create client from config
-            info!("Initializing client");
-            let client: Client = Client::try_from(config)?;
-            client.initialize()?;
+        // Initialize network stack and connect.
+        info!("Connecting to network");
+        client.connect()?;
 
-            // Initialize RPC server
-            if let Some(rpc_config) = rpc_config {
-                use nimiq::extras::rpc_server::initialize_rpc_server;
-                let rpc_server = initialize_rpc_server(&client, rpc_config)
-                    .expect("Failed to initialize RPC server");
-                tokio::spawn(rpc_server.into_future());
-            }
+        Ok(client)
+    });
 
-            // Initialize metrics server
-            if let Some(metrics_config) = metrics_config {
-                use nimiq::config::config::ProtocolConfig;
-                use nimiq::extras::metrics_server::initialize_metrics_server;
-                if let ProtocolConfig::Wss {
-                    pkcs12_key_file,
-                    pkcs12_passphrase,
-                    ..
-                } = protocol_config
-                {
-                    let pkcs12_key_file = pkcs12_key_file.to_str().unwrap_or_else(|| {
-                        panic!(
-                            "Failed to convert path to PKCS#12 key file to string: {}",
-                            pkcs12_key_file.display()
-                        )
-                    });
-                    let metrics_server = initialize_metrics_server(
-                        &client,
-                        metrics_config,
-                        pkcs12_key_file,
-                        &pkcs12_passphrase,
-                    )
-                    .expect("Failed to initialize metrics server");
-                    tokio::spawn(metrics_server.into_future());
-                } else {
-                    error!("Cannot provide metrics when running without a certificate");
+    // Create the "monitor" future which never completes to keep the client alive.
+    // This closure is executed after the client has been initialized.
+    // TODO Get rid of this. Make the Client a future instead.
+    let create_monitor_future = move |client: Client| {
+        let mut statistics_interval = config_file.log.statistics;
+        let mut show_statistics = true;
+        if statistics_interval == 0 {
+            statistics_interval = 10;
+            show_statistics = false;
+        }
+
+        // Run this periodically and optionally show some info.
+        tokio::time::interval(Duration::from_secs(statistics_interval))
+            .for_each(move |_| {
+                if show_statistics {
+                    let peer_count = client.network().connections.peer_count();
+                    let head = client.blockchain().head().clone();
+                    info!(
+                        "Head: #{} - {}, Peers: {}",
+                        head.block_number(),
+                        head.hash(),
+                        peer_count
+                    );
                 }
-            }
+                future::ready(())
+            })
+            .map(|_| Ok(()))
+    };
 
-            // Initialize Websocket RPC server
-            // TODO: Configuration
-            if let Some(ws_rpc_config) = ws_rpc_config {
-                use nimiq::extras::ws_rpc_server::initialize_ws_rcp_server;
-                let ws_rpc_server = initialize_ws_rcp_server(&client, ws_rpc_config)
-                    .expect("Failed to initialize websocket RPC server");
-                tokio::spawn(ws_rpc_server.into_future());
-            }
-
-            // Initialize network stack and connect
-            info!("Connecting to network");
-
-            client.connect()?;
-
-            // The Nimiq client is now running and we can access it trough the `client` object.
-
-            // TODO: RPC server and metrics server need to be instantiated here
-            Ok(client)
-        })
-        .and_then(move |client| {
-            // NOTE: This is the "monitor" future, which keeps the Client object alive.
-
-            let mut statistics_interval = config_file.log.statistics;
-            let mut show_statistics = true;
-            if statistics_interval == 0 {
-                statistics_interval = 10;
-                show_statistics = false;
-            }
-
-            // Run this periodically and optionally show some info
-            Interval::new_interval(Duration::from_secs(statistics_interval))
-                .map_err(|e| panic!("Timer failed: {}", e))
-                .for_each(move |_| {
-                    if show_statistics {
-                        let peer_count = client.network().connections.peer_count();
-                        let head = client.blockchain().head().clone();
-                        info!(
-                            "Head: #{} - {}, Peers: {}",
-                            head.block_number(),
-                            head.hash(),
-                            peer_count
-                        );
-                    }
-
-                    future::ok::<(), Error>(())
-                })
-        })
-        .map_err(|e: Error| warn!("{}", e)),
-    );
-
-    Ok(())
+    // Start the tokio runtime.
+    let mut runtime = Runtime::new()?;
+    runtime.block_on(init_future.and_then(create_monitor_future))
 }
 
 fn main() {
