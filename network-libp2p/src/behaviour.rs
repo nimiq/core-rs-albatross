da4a7556d52eea139bddf33e6cddb374b8cb2fb5
Sergio Valverde <sergio@nimiq.com>
2020-10-25 22:26:09 -0600

    WIP: Create network metabehaviour

--- network-libp2p/src/behaviour.rs
+++ network-libp2p/src/behaviour.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 
@@ -6,12 +6,13 @@ use futures::channel::mpsc;
 use futures::task::{Context, Poll};
 use futures::{ready, StreamExt};
 use ip_network::IpNetwork;
+use libp2p::NetworkBehaviour;
 use libp2p::core::connection::ConnectionId;
 use libp2p::core::multiaddr::Protocol;
 use libp2p::core::ConnectedPoint;
 use libp2p::core::Multiaddr;
 use libp2p::swarm::{
-    NetworkBehaviour, NetworkBehaviourAction, NotifyHandler, PollParameters, ProtocolsHandler,
+    NetworkBehaviour, NetworkBehaviourAction, NetworkBehaviourEventProcess, NotifyHandler, PollParameters, ProtocolsHandler,
 };
 use libp2p::PeerId;
 
@@ -30,34 +31,37 @@ const IPV6_SUBNET_MASK: u8 = 96;
 
 const DEFAULT_BAN_TIME: Duration = Duration::from_secs(60 * 10);
 
+#[derive(NetworkBehaviour)]
 pub struct NimiqBehaviour {
-    peers: HashMap<PeerId, Arc<Peer>>,
+    pub message_behaviour: MessageBehaviour,
+    pub limit_behaviour: LimitBehaviour,
+}
+
+impl NetworkBehaviourEventProcess<NetworkEvent<Peer>> for NimiqBehaviour {
+    fn inject_event(&mut self, message: NetworkEvent<Peer>) {}
+}
+
+pub struct LimitBehaviour {
     ip_ban: HashMap<IpNetwork, SystemTime>,
     ip_count: HashMap<IpNetwork, usize>,
     ipv4_count: usize,
     ipv6_count: usize,
     events: VecDeque<NetworkEvent<Peer>>,
-    peer_tx: mpsc::Sender<PeerAction>,
-    peer_rx: mpsc::Receiver<PeerAction>,
 }
 
-impl NimiqBehaviour {
+impl LimitBehaviour {
     pub fn new() -> Self {
-        let (peer_tx, peer_rx) = mpsc::channel(4096);
         Self {
-            peers: HashMap::new(),
             ip_ban: HashMap::new(),
             ip_count: HashMap::new(),
             ipv4_count: 0,
             ipv6_count: 0,
             events: VecDeque::new(),
-            peer_tx,
-            peer_rx,
         }
     }
 }
 
-impl NetworkBehaviour for NimiqBehaviour {
+impl NetworkBehaviour for LimitBehaviour {
     type ProtocolsHandler = NimiqHandler;
     type OutEvent = NetworkEvent<Peer>;
 
@@ -103,7 +107,7 @@ impl NetworkBehaviour for NimiqBehaviour {
             debug!("IPv6 subnet limit reached");
             close_connection = true;
         }
-        if PEER_COUNT_MAX < self.peers.keys().len() + 1 {
+        if PEER_COUNT_MAX < self.ipv4_count + self.ipv6_count + 1 {
             debug!("Max peer connections limit reached");
             close_connection = true;
         }
@@ -123,14 +127,10 @@ impl NetworkBehaviour for NimiqBehaviour {
         }
 
         // Send the network event to the Swarm
-        let peer = Arc::new(Peer::new(peer_id.clone(), self.peer_tx.clone()));
+        let (peer_tx, peer_rx) = mpsc::channel(4096);
+        let peer = Arc::new(Peer::new(peer_id.clone(), peer_tx));
         if close_connection {
             self.events.push_back(NetworkEvent::PeerDisconnect(peer));
-        } else {
-            self.peers
-                .insert(peer_id.clone(), Arc::clone(&peer))
-                .map(|p| panic!("Duplicate peer {}", p.id));
-            self.events.push_back(NetworkEvent::PeerJoined(peer));
         }
     }
 
@@ -144,12 +144,6 @@ impl NetworkBehaviour for NimiqBehaviour {
         conn: &ConnectionId,
         info: &ConnectedPoint,
     ) {
-        let peer = self
-            .peers
-            .remove(peer_id)
-            .expect("Unknown peer disconnected");
-        self.events.push_back(NetworkEvent::PeerLeft(peer.clone()));
-
         let address = match info {
             ConnectedPoint::Listener { send_back_addr, .. } => send_back_addr.clone(),
             ConnectedPoint::Dialer { address } => address.clone(),
@@ -162,9 +156,9 @@ impl NetworkBehaviour for NimiqBehaviour {
             _ => return,
         };
 
-        if peer.banned {
-            self.ip_ban.insert(ip, SystemTime::now() + DEFAULT_BAN_TIME);
-        }
+        // if peer.banned {
+        //     self.ip_ban.insert(ip, SystemTime::now() + DEFAULT_BAN_TIME);
+        // }
 
         // Decrement peer counts per IP
         *self.ip_count.entry(ip).or_insert(1) -= 1;
@@ -177,6 +171,81 @@ impl NetworkBehaviour for NimiqBehaviour {
         } else {
             self.ipv6_count -= 1;
         }
+    }
+
+    fn inject_event(
+        &mut self,
+        peer_id: PeerId,
+        _connection: ConnectionId,
+        msg: <Self::ProtocolsHandler as ProtocolsHandler>::OutEvent,
+    ) {}
+
+    fn poll(
+        &mut self,
+        cx: &mut Context<'_>,
+        _params: &mut impl PollParameters,
+    ) -> Poll<NetworkBehaviourAction<NimiqHandlerAction, NetworkEvent<Peer>>> {
+        let ip_ban = self.ip_ban.clone();
+        for (ip, time) in ip_ban {
+            if time < SystemTime::now() {
+                self.ip_ban.remove(&ip);
+            }
+        }
+
+        // Emit custom events.
+        if let Some(event) = self.events.pop_front() {
+            return Poll::Ready(NetworkBehaviourAction::GenerateEvent(event));
+        }
+
+        Poll::Pending
+    }
+}
+
+pub struct MessageBehaviour {
+    peers: HashMap<PeerId, Arc<Peer>>,
+    events: VecDeque<NetworkEvent<Peer>>,
+    peer_tx: mpsc::Sender<PeerAction>,
+    peer_rx: mpsc::Receiver<PeerAction>,
+}
+
+impl MessageBehaviour {
+    pub fn new() -> Self {
+        let (peer_tx, peer_rx) = mpsc::channel(4096);
+        Self {
+            peers: HashMap::new(),
+            events: VecDeque::new(),
+            peer_tx,
+            peer_rx,
+        }
+    }
+}
+
+impl NetworkBehaviour for MessageBehaviour {
+    type ProtocolsHandler = NimiqHandler;
+    type OutEvent = NetworkEvent<Peer>;
+
+    fn new_handler(&mut self) -> Self::ProtocolsHandler {
+        NimiqHandler::new()
+    }
+
+    fn addresses_of_peer(&mut self, _peer_id: &PeerId) -> Vec<Multiaddr> {
+        Vec::new()
+    }
+
+    fn inject_connected(&mut self, peer_id: &PeerId) {
+        let peer = Arc::new(Peer::new(peer_id.clone(), self.peer_tx.clone()));
+        self.peers
+            .insert(peer_id.clone(), Arc::clone(&peer))
+            .map(|p| panic!("Duplicate peer {}", p.id));
+        self.events.push_back(NetworkEvent::PeerJoined(peer));
+    }
+
+    fn inject_disconnected(&mut self, peer_id: &PeerId) {
+        let peer = self
+            .peers
+            .remove(peer_id)
+            .expect("Unknown peer disconnected");
+        self.events.push_back(NetworkEvent::PeerLeft(peer.clone()));
     }
 
     fn inject_event(
@@ -197,13 +266,6 @@ impl NetworkBehaviour for NimiqBehaviour {
         cx: &mut Context<'_>,
         _params: &mut impl PollParameters,
     ) -> Poll<NetworkBehaviourAction<NimiqHandlerAction, NetworkEvent<Peer>>> {
-        let ip_ban = self.ip_ban.clone();
-        for (ip, time) in ip_ban {
-            if time < SystemTime::now() {
-                self.ip_ban.remove(&ip);
-            }
-        }
-
         // Emit custom events.
         if let Some(event) = self.events.pop_front() {
             return Poll::Ready(NetworkBehaviourAction::GenerateEvent(event));
