4f5cbf7d6d4eb2f0775c7c20d11aaf32478c5088
Smitty <me@smitop.com>
2021-04-05 10:39:44 -0400

    Update message

--- lib/src/config/config_file/mod.rs
+++ lib/src/config/config_file/mod.rs
@@ -100,8 +100,9 @@ impl ConfigFile {
         let path = paths::home().join("client.toml");
         if !path.exists() {
             let msg = format!(
-                "Config file not found at {}. An example config file can be found at lib/src/config/config_file/client.example.toml",
-                path.display()
+                "Config file not found. Please create one at {} or specify a location using -c path/to/config.toml, see the example config file at {}",
+                path.display(),
+                path_example.display(),
             );
             log::warn!("{}", msg);
             return Err(Error::config_error(&msg));
