ae13b7714227dd91fb11ce01edfbd2ecf1ef474a
Sören <hello@soerenschwert.de>
2021-05-26 14:14:22 +0200

    Disable forced Docker-setup log file
    It's not needed anymore with the rotating trace logs

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-PIPE="/home/nimiq/nimiq.log.pipe"
-
-mkfifo $PIPE || true
-cat $PIPE &
 mkdir -p /home/nimiq/.nimiq
 
 if [[ -z "$NIMIQ_HOST" ]]; then
@@ -15,7 +11,5 @@ fi
 if [[ ! -e "/home/nimiq/.nimiq/client.toml" || $OVERRIDE_CONFIG_FILE -eq 1 ]]; then
     ./docker_config.sh > /home/nimiq/.nimiq/client.toml
 fi
-
-#cat /home/nimiq/.nimiq/client.toml > $PIPE
 
 /usr/local/bin/nimiq-client $@
