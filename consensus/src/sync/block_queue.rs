471fc0a2991b8ff042edf1eed3a70dba960193dc
styppo <mail@styp-rekowsky.de>
2021-07-27 02:41:46 +0200

    BlockQueue: Run blockchain operations on separate thread

--- consensus/src/sync/block_queue.rs
+++ consensus/src/sync/block_queue.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+use std::task::Waker;
 use std::{
     collections::{BTreeMap, HashSet},
     pin::Pin,
@@ -5,22 +7,26 @@ use std::{
     task::{Context, Poll},
 };
 
+use futures::future::BoxFuture;
 use futures::stream::{BoxStream, Stream, StreamExt};
+use futures::FutureExt;
 use pin_project::pin_project;
+use tokio::task::spawn_blocking;
 
+use blockchain::AbstractBlockchain;
 use network_interface::{
     network::{MsgAcceptance, Network, PubsubId, Topic},
     peer::Peer,
 };
 use nimiq_block::Block;
-use nimiq_blockchain::{Blockchain, PushResult};
+use nimiq_blockchain::{Blockchain, PushError, PushResult};
 use nimiq_hash::Blake2bHash;
 use nimiq_primitives::policy;
 
-use super::request_component::RequestComponent;
 use crate::consensus_agent::ConsensusAgent;
 use crate::sync::request_component::RequestComponentEvent;
-use blockchain::AbstractBlockchain;
+
+use super::request_component::RequestComponent;
 
 #[derive(Clone, Debug, Default)]
 pub struct BlockTopic;
@@ -81,29 +87,36 @@ struct Inner<N: Network> {
     ///  - The inner `Vec` should really be a `SmallVec<[Block; 1]>` or similar.
     ///
     buffer: BTreeMap<u32, Vec<Block>>,
+
+    /// Vector of pending `blockchain.push()` operations.
+    push_ops: VecDeque<BoxFuture<'static, PushOpResult>>,
+
+    waker: Option<Waker>,
+}
+
+enum PushOpResult {
+    Single(Result<PushResult, PushError>),
+    Multi(Result<PushResult, PushError>, HashSet<Blake2bHash>),
 }
 
 impl<N: Network> Inner<N> {
-    /// Handles a block announcement and returns true if the block has successfully extended
-    /// the blockchain.
+    /// Handles a block announcement.
     fn on_block_announced<TReq: RequestComponent<N::PeerType>>(
         &mut self,
         block: Block,
         mut request_component: Pin<&mut TReq>,
         peer_id: <N::PeerType as Peer>::Id,
         pubsub_id: Option<<N as Network>::PubsubId>,
-    ) -> bool {
+    ) {
         let block_height = block.block_number();
         let head_height = self.blockchain.block_number();
 
         if block_height <= head_height {
             // Fork block
-            return self.push_block(block, pubsub_id);
+            self.push_block(block, pubsub_id);
         } else if block_height == head_height + 1 {
             // New head block
-            let result = self.push_block(block, pubsub_id);
-            self.push_buffered();
-            return result;
+            self.push_block(block, pubsub_id);
         } else if block_height > head_height + self.config.window_max {
             log::warn!(
                 "Discarding block #{} outside of buffer window (max {}).",
@@ -129,7 +142,7 @@ impl<N: Network> Inner<N> {
             // Put block inside buffer window
             self.insert_into_buffer(block);
 
-            log::trace!("Requesting missing blocks: target_hash = {}, head_hash = {}, prev_macro_block_height = {}", block_hash, head_hash, prev_macro_block_height);
+            log::debug!("Requesting missing blocks: target_hash = {}, head_hash = {}, prev_macro_block_height = {}", block_hash, head_hash, prev_macro_block_height);
             log::trace!(
                 "n = {} = {} - {} + 1",
                 head_height - prev_macro_block_height + 1,
@@ -155,141 +168,139 @@ impl<N: Network> Inner<N> {
 
             request_component.request_missing_blocks(block_hash, block_locators);
         }
-
-        false
     }
 
     fn on_missing_blocks_received(&mut self, blocks: Vec<Block>) {
-        let mut it = blocks.into_iter();
+        if blocks.is_empty() {
+            log::debug!("Received empty missing blocks response");
+            return;
+        }
 
-        // Hashes of invalid blocks
-        let mut invalid_blocks = HashSet::new();
+        let blockchain = Arc::clone(&self.blockchain);
+        let future = async move {
+            let mut block_iter = blocks.into_iter();
 
-        // Try to push blocks, until we encounter an inferior or invalid block
-        #[allow(clippy::while_let_on_iterator)]
-        while let Some(block) = it.next() {
-            let block_hash = block.hash();
+            // Hashes of invalid blocks
+            let mut invalid_blocks = HashSet::new();
 
-            log::trace!(
-                "Pushing block #{} from missing blocks response",
-                block.block_number()
-            );
+            // Initialize push_result to some random value.
+            // It is always overwritten in the first loop iteration.
+            let mut push_result = Err(PushError::Orphan);
 
-            match self.blockchain.push(block) {
-                Ok(PushResult::Ignored) => {
-                    log::warn!("Inferior chain - Aborting");
-                    invalid_blocks.insert(block_hash);
-                    break;
-                }
-                Err(e) => {
-                    log::warn!("Failed to push block: {}", e);
-                    invalid_blocks.insert(block_hash);
-                    break;
-                }
-                Ok(result) => {
-                    log::trace!("Block pushed: {:?}", result);
+            // Try to push blocks, until we encounter an inferior or invalid block
+            #[allow(clippy::while_let_on_iterator)]
+            while let Some(block) = block_iter.next() {
+                let block_hash = block.hash();
+
+                log::debug!(
+                    "Pushing block #{} from missing blocks response",
+                    block.block_number()
+                );
+
+                let blockchain1 = Arc::clone(&blockchain);
+                push_result = spawn_blocking(move || blockchain1.push(block))
+                    .await
+                    .expect("blockchain.push() should not panic");
+                match &push_result {
+                    Ok(PushResult::Ignored) => {
+                        log::warn!("Inferior chain - Aborting");
+                        invalid_blocks.insert(block_hash);
+                        break;
+                    }
+                    Err(e) => {
+                        log::warn!("Failed to push missing block: {}", e);
+                        invalid_blocks.insert(block_hash);
+                        break;
+                    }
+                    Ok(result) => {
+                        log::debug!("Missing block pushed: {:?}", result);
+                    }
                 }
             }
-        }
 
-        // If there are remaining blocks in the iterator, those are invalid.
-        it.for_each(|block| {
-            invalid_blocks.insert(block.hash());
-        });
-
-        if !invalid_blocks.is_empty() {
-            log::trace!("Removing any blocks that depend on: {:?}", invalid_blocks);
-
-            // Iterate over all offsets, remove element if no blocks remain at that offset.
-            self.buffer.drain_filter(|_block_number, blocks| {
-                // Iterate over all blocks at an offset, remove block, if parent is invalid
-                blocks.drain_filter(|block| {
-                    if invalid_blocks.contains(block.parent_hash()) {
-                        log::trace!("Removing block because parent is invalid: {}", block.hash());
-                        invalid_blocks.insert(block.hash());
-                        true
-                    } else {
-                        false
-                    }
-                });
-                blocks.is_empty()
+            // If there are remaining blocks in the iterator, those are invalid.
+            block_iter.for_each(|block| {
+                invalid_blocks.insert(block.hash());
             });
-        }
 
-        // We might be able to push buffered blocks now
-        self.push_buffered();
+            PushOpResult::Multi(push_result, invalid_blocks)
+        };
+
+        self.push_ops.push_back(future.boxed());
+        if let Some(waker) = &self.waker {
+            waker.wake_by_ref();
+        }
     }
 
     /// Pushes the block to the blockchain and returns whether it has extended the blockchain.
-    fn push_block(&mut self, block: Block, pubsub_id: Option<<N as Network>::PubsubId>) -> bool {
-        let acceptance;
-        let extended = match self.blockchain.push(block) {
-            Ok(result) => {
-                log::trace!("Block pushed: {:?}", result);
-
-                acceptance = match result {
-                    PushResult::Known | PushResult::Extended | PushResult::Rebranched => {
-                        MsgAcceptance::Accept
+    fn push_block(&mut self, block: Block, pubsub_id: Option<<N as Network>::PubsubId>) {
+        let blockchain = Arc::clone(&self.blockchain);
+        let network = Arc::clone(&self.network);
+        let future = async move {
+            let push_result = spawn_blocking(move || blockchain.push(block))
+                .await
+                .expect("blockchain.push() should not panic");
+            let acceptance = match &push_result {
+                Ok(result) => {
+                    log::debug!("Block pushed: {:?}", result);
+                    match result {
+                        PushResult::Known | PushResult::Extended | PushResult::Rebranched => {
+                            MsgAcceptance::Accept
+                        }
+                        PushResult::Forked | PushResult::Ignored => MsgAcceptance::Ignore,
                     }
-                    PushResult::Forked | PushResult::Ignored => MsgAcceptance::Ignore,
-                };
+                }
+                Err(e) => {
+                    log::warn!("Failed to push block: {}", e);
+                    // TODO Ban peer
+                    MsgAcceptance::Reject
+                }
+            };
 
-                result == PushResult::Extended
-            }
-            Err(e) => {
-                log::warn!("Failed to push block: {}", e);
-
-                acceptance = MsgAcceptance::Reject;
-
-                false
-            }
-        };
-
-        // Let the network layer know if it should relay the message this block came from
-        if let Some(pubsub_id) = pubsub_id {
-            let network1 = Arc::clone(&self.network);
-            tokio::spawn(async move {
-                match network1.validate_message(pubsub_id, acceptance).await {
+            // Let the network layer know if it should relay the message this block came from
+            if let Some(pubsub_id) = pubsub_id {
+                match network.validate_message(pubsub_id, acceptance).await {
                     Ok(true) => log::trace!("The block message was relayed succesfully"),
                     Ok(false) => log::warn!("Validation took too long: the block message was no longer in the message cache"),
                     Err(e) => log::error!("Network error while relaying block message: {}", e),
                 };
-            });
+            };
+
+            PushOpResult::Single(push_result)
         };
 
-        extended
+        self.push_ops.push_back(future.boxed());
+        if let Some(waker) = &self.waker {
+            waker.wake_by_ref();
+        }
     }
 
     fn push_buffered(&mut self) {
-        loop {
-            let head_height = self.blockchain.block_number();
-            log::trace!("head_height = {}", head_height);
+        let head_height = self.blockchain.block_number();
+        log::trace!("head_height = {}", head_height);
 
-            // Check if queued block can be pushed to block chain
-            if let Some(entry) = self.buffer.first_entry() {
-                log::trace!("first entry: {:?}", entry);
+        // Check if queued block can be pushed to block chain
+        if let Some(entry) = self.buffer.first_entry() {
+            log::trace!("first entry: {:?}", entry);
 
-                if *entry.key() > head_height + 1 {
-                    break;
-                }
+            if *entry.key() > head_height + 1 {
+                return;
+            }
 
-                // Pop block from queue
-                let (_, blocks) = entry.remove_entry();
+            // Pop block from queue
+            let (_, blocks) = entry.remove_entry();
 
-                // If we get a Vec from the BTree, it must not be empty
-                assert!(!blocks.is_empty());
+            // If we get a Vec from the BTree, it must not be empty
+            assert!(!blocks.is_empty());
 
-                for block in blocks {
-                    log::trace!(
-                        "Pushing block #{} (currently at #{}, {} blocks left)",
-                        block.block_number(),
-                        head_height,
-                        self.buffer.len(),
-                    );
-                    self.push_block(block, None);
-                }
-            } else {
-                break;
+            for block in blocks {
+                log::trace!(
+                    "Pushing block #{} (currently at #{}, {} blocks left)",
+                    block.block_number(),
+                    head_height,
+                    self.buffer.len(),
+                );
+                self.push_block(block, None);
             }
         }
     }
@@ -299,6 +310,64 @@ impl<N: Network> Inner<N> {
             .entry(block.block_number())
             .or_default()
             .push(block)
+    }
+
+    fn remove_invalid_blocks(&mut self, mut invalid_blocks: HashSet<Blake2bHash>) {
+        if invalid_blocks.is_empty() {
+            return;
+        }
+
+        log::trace!("Removing any blocks that depend on: {:?}", invalid_blocks);
+
+        // Iterate over all offsets, remove element if no blocks remain at that offset.
+        self.buffer.drain_filter(|_block_number, blocks| {
+            // Iterate over all blocks at an offset, remove block, if parent is invalid
+            blocks.drain_filter(|block| {
+                if invalid_blocks.contains(block.parent_hash()) {
+                    log::trace!("Removing block because parent is invalid: {}", block.hash());
+                    invalid_blocks.insert(block.hash());
+                    true
+                } else {
+                    false
+                }
+            });
+            blocks.is_empty()
+        });
+    }
+}
+
+impl<N: Network> Stream for Inner<N> {
+    type Item = ();
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        // Store waker.
+        match &mut self.waker {
+            Some(waker) if !waker.will_wake(cx.waker()) => *waker = cx.waker().clone(),
+            None => self.waker = Some(cx.waker().clone()),
+            _ => {}
+        };
+
+        if let Some(op) = self.push_ops.front_mut() {
+            let result = ready!(op.poll_unpin(cx));
+            self.push_ops.pop_front().expect("PushOp should be present");
+
+            match result {
+                PushOpResult::Single(Ok(result))
+                    if result == PushResult::Extended || result == PushResult::Rebranched =>
+                {
+                    self.push_buffered();
+                    return Poll::Ready(Some(()));
+                }
+                PushOpResult::Multi(result, invalid_blocks) => {
+                    self.remove_invalid_blocks(invalid_blocks);
+                    self.push_buffered();
+                    return Poll::Ready(Some(()));
+                }
+                _ => {}
+            };
+        }
+
+        Poll::Pending
     }
 }
 
@@ -352,6 +421,8 @@ impl<N: Network, TReq: RequestComponent<N::PeerType>> BlockQueue<N, TReq> {
                 blockchain,
                 network,
                 buffer,
+                push_ops: VecDeque::new(),
+                waker: None,
             },
             accepted_announcements: 0,
         }
@@ -390,39 +461,39 @@ impl<N: Network, TReq: RequestComponent<N::PeerType>> Stream for BlockQueue<N, T
         let num_peers = self.num_peers();
         let this = self.project();
 
-        // Note: I think it doesn't matter what is done first
+        // First, advance the internal future to process buffered blocks.
+        match this.inner.poll_next_unpin(cx) {
+            Poll::Ready(Some(_)) => {
+                *this.accepted_announcements = this.accepted_announcements.saturating_add(1);
+                return Poll::Ready(Some(BlockQueueEvent::ReceivedBlocks));
+            }
+            Poll::Ready(None) => unreachable!(),
+            Poll::Pending => {}
+        }
 
-        // First, try to get as many blocks from the gossipsub stream as possible
+        // Then, try to get as many blocks from the gossipsub stream as possible.
         match this.block_stream.poll_next(cx) {
             Poll::Ready(Some((block, pubsub_id))) => {
                 // Ignore all block announcements until there is at least once synced peer.
                 if num_peers > 0 {
-                    let result = this.inner.on_block_announced(
+                    this.inner.on_block_announced(
                         block,
                         this.request_component,
                         pubsub_id.propagation_source(),
                         Some(pubsub_id),
                     );
-                    if result {
-                        *this.accepted_announcements =
-                            this.accepted_announcements.saturating_add(1);
-                    }
-
-                    return Poll::Ready(Some(BlockQueueEvent::ReceivedBlocks));
                 }
+                return Poll::Pending;
             }
-
-            // If the block_stream is exhausted, we quit as well
+            // If the block_stream is exhausted, we quit as well.
             Poll::Ready(None) => return Poll::Ready(None),
-
             Poll::Pending => {}
         }
 
-        // Then, read all the responses we got for our missing blocks requests
+        // Then, read all the responses we got for our missing blocks requests.
         match this.request_component.poll_next(cx) {
             Poll::Ready(Some(RequestComponentEvent::ReceivedBlocks(blocks))) => {
                 this.inner.on_missing_blocks_received(blocks);
-                return Poll::Ready(Some(BlockQueueEvent::ReceivedBlocks));
             }
             Poll::Ready(Some(RequestComponentEvent::PeerMacroSynced(peer))) => {
                 return Poll::Ready(Some(BlockQueueEvent::PeerMacroSynced(peer)));
