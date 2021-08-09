468a9b2e6e051bad293a10972b51a1db86392d12
brunoffranca <bruno@franca.xyz>
2021-08-09 19:05:16 +0100

    In the primitives/account crate: 1. Fixed a bug where we were not checking that the retire/reactivate/unpark transactions are signed with the warm address. 2. Remove the grace period for parked validators. They are now retired at the end of the epoch when they were parked. 3. Added some documentation.

--- primitives/account/src/staking_contract/receipts.rs
+++ primitives/account/src/staking_contract/receipts.rs
@@ -5,9 +5,9 @@ use nimiq_bls::CompressedPublicKey as BlsPublicKey;
 use nimiq_hash::Blake2bHash;
 use nimiq_keys::Address;
 
-/// A receipt for slash inherents. It shows whether a given slot or validator was newly disabled,
-/// lost rewards or parked by a specific slash inherent. This is necessary to be able to revert
-/// slash inherents.
+/// A collection of receipts for inherents/transactions. This is necessary to be able to revert
+/// those inherents/transactions.
+
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct SlashReceipt {
     pub newly_parked: bool,
@@ -25,8 +25,7 @@ pub struct UpdateValidatorReceipt {
 
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct RetireValidatorReceipt {
-    pub current_epoch_parking: bool,
-    pub previous_epoch_parking: bool,
+    pub parked_set: bool,
 }
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, Eq, PartialEq)]
@@ -36,8 +35,7 @@ pub struct ReactivateValidatorReceipt {
 
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct UnparkValidatorReceipt {
-    pub current_epoch_parking: bool,
-    pub previous_epoch_parking: bool,
+    pub parked_set: bool,
     #[beserial(len_type(u16))]
     pub current_disabled_slots: Option<BTreeSet<u16>>,
     #[beserial(len_type(u16))]
