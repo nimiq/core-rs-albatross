488058aedcc8043491efb954c8d3158dedfd4d64
brunoffranca <bruno@franca.xyz>
2021-08-19 03:43:32 +0100

    Fixed build-tools crate. It now builds.

--- build-tools/src/genesis/mod.rs
+++ build-tools/src/genesis/mod.rs
@@ -7,7 +7,7 @@ use chrono::{DateTime, Utc};
 use thiserror::Error;
 use toml::de::Error as TomlError;
 
-use account::{Account, AccountError, AccountsList, BasicAccount, StakingContract};
+use account::{Account, AccountError, Accounts, AccountsList, BasicAccount, StakingContract};
 use beserial::{Deserialize, Serialize, SerializingError};
 use block::{Block, MacroBlock, MacroBody, MacroHeader};
 use bls::{PublicKey as BlsPublicKey, SecretKey as BlsSecretKey};
@@ -21,14 +21,11 @@ use vrf::VrfSeed;
 mod config;
 
 const DEFAULT_SIGNING_KEY: [u8; 96] = [0u8; 96];
-const DEFAULT_STAKING_CONTRACT_ADDRESS: &str = "NQ38 STAK 1NG0 0000 0000 C0NT RACT 0000 0000";
 
 #[derive(Debug, Error)]
 pub enum GenesisBuilderError {
     #[error("No signing key to generate genesis seed.")]
     NoSigningKey,
-    #[error("No staking contract address.")]
-    NoStakingContractAddress,
     #[error("Invalid timestamp: {0}")]
     InvalidTimestamp(DateTime<Utc>),
     #[error("Serialization failed")]
@@ -55,9 +52,8 @@ pub struct GenesisBuilder {
     pub seed_message: Option<String>,
     pub timestamp: Option<DateTime<Utc>>,
     pub validators: Vec<config::GenesisValidator>,
-    pub stakes: Vec<config::GenesisStake>,
+    pub stakers: Vec<config::GenesisStaker>,
     pub accounts: Vec<config::GenesisAccount>,
-    pub staking_contract_address: Option<Address>,
 }
 
 impl GenesisBuilder {
@@ -67,9 +63,8 @@ impl GenesisBuilder {
             seed_message: None,
             timestamp: None,
             validators: vec![],
-            stakes: vec![],
+            stakers: vec![],
             accounts: vec![],
-            staking_contract_address: None,
         }
     }
 
@@ -81,8 +76,6 @@ impl GenesisBuilder {
 
     pub fn with_defaults(&mut self) -> &mut Self {
         self.signing_key = Some(BlsSecretKey::deserialize_from_vec(&DEFAULT_SIGNING_KEY).unwrap());
-        self.staking_contract_address =
-            Some(Address::from_user_friendly_address(DEFAULT_STAKING_CONTRACT_ADDRESS).unwrap());
         self
     }
 
@@ -101,23 +94,18 @@ impl GenesisBuilder {
         self
     }
 
-    pub fn with_staking_contract_address(&mut self, address: Address) -> &mut Self {
-        self.staking_contract_address = Some(address);
-        self
-    }
-
     pub fn with_genesis_validator(
         &mut self,
-        validator_id: ValidatorId,
+        validator_address: Address,
+        warm_address: Address,
         validator_key: BlsPublicKey,
         reward_address: Address,
-        balance: Coin,
     ) -> &mut Self {
         self.validators.push(config::GenesisValidator {
-            validator_address: validator_id,
-            reward_address,
-            balance,
+            validator_address,
+            warm_address,
             validator_key,
+            reward_address,
         });
         self
     }
@@ -125,13 +113,13 @@ impl GenesisBuilder {
     pub fn with_genesis_stake(
         &mut self,
         staker_address: Address,
-        validator_id: ValidatorId,
+        validator_address: Address,
         balance: Coin,
     ) -> &mut Self {
-        self.stakes.push(config::GenesisStake {
+        self.stakers.push(config::GenesisStaker {
             staker_address,
             balance,
-            validator_id,
+            delegation: validator_address,
         });
         self
     }
@@ -153,84 +141,80 @@ impl GenesisBuilder {
             mut validators,
             mut stakes,
             mut accounts,
-            staking_contract,
         } = toml::from_str(&read_to_string(path)?)?;
 
         signing_key.map(|skey| self.with_signing_key(skey));
         seed_message.map(|msg| self.with_seed_message(msg));
         timestamp.map(|t| self.with_timestamp(t));
-        staking_contract.map(|address| self.with_staking_contract_address(address));
         self.validators.append(&mut validators);
-        self.stakes.append(&mut stakes);
+        self.stakers.append(&mut stakes);
         self.accounts.append(&mut accounts);
 
         Ok(self)
     }
 
     pub fn generate(&self) -> Result<GenesisInfo, GenesisBuilderError> {
+        // Initialize the environment.
+        let env = VolatileEnvironment::new(10)?;
+        let mut txn = WriteTransaction::new(&env);
         let timestamp = self.timestamp.unwrap_or_else(Utc::now);
+
+        // Initialize the accounts.
+        let accounts = Accounts::new(env.clone());
+
+        debug!("Staking contract");
+        self.generate_staking_contract(&accounts, &mut txn)?;
+
+        debug!("Genesis accounts");
+        let mut genesis_accounts: Vec<(Address, Account)> = Vec::new();
+
+        for genesis_account in &self.accounts {
+            let address = genesis_account.address.clone();
+
+            let account = Account::Basic(BasicAccount {
+                balance: genesis_account.balance,
+            });
+
+            genesis_accounts.push((address, account));
+        }
+
+        accounts.init(&mut txn, genesis_accounts.clone());
 
         // generate seeds
         let signing_key = self
             .signing_key
             .as_ref()
             .ok_or(GenesisBuilderError::NoSigningKey)?;
+
         // random message used as seed for VRF that generates pre-genesis seed
         let seed_message = self.seed_message.clone().unwrap_or_else(|| {
             "love ai amor mohabbat hubun cinta lyubov bhalabasa amour kauna pi'ara liebe eshq upendo prema amore katresnan sarang anpu prema yeu".to_string()
         });
+
         // pre-genesis seed (used for slot selection)
         let pre_genesis_seed: VrfSeed = signing_key
             .sign_hash(Blake2sHasher::new().digest(seed_message.as_bytes()))
             .compress()
             .into();
         debug!("Pre genesis seed: {}", pre_genesis_seed);
+
         // seed of genesis block = VRF(seed_0)
         let seed = pre_genesis_seed.sign_next(signing_key);
         debug!("Genesis seed: {}", seed);
 
-        // generate staking contract
-        let staking_contract = self.generate_staking_contract()?;
-        debug!("Staking contract: {:#?}", staking_contract);
-
         // generate slot allocation from staking contract
-        let slots = staking_contract.select_validators(&seed);
+        let slots = StakingContract::select_validators(&accounts.tree, &mut txn, &seed);
         debug!("Slots: {:#?}", slots);
 
         // Body
         let mut body = MacroBody::new();
         body.validators = Some(slots);
+
         let body_root = body.hash::<Blake2bHash>();
         debug!("Body root: {}", &body_root);
 
-        // accounts
-        let mut genesis_accounts: Vec<(Address, Account)> = Vec::new();
-        genesis_accounts.push((
-            Address::clone(
-                self.staking_contract_address
-                    .as_ref()
-                    .ok_or(GenesisBuilderError::NoStakingContractAddress)?,
-            ),
-            Account::Staking(staking_contract),
-        ));
-        for genesis_account in &self.accounts {
-            let address = genesis_account.address.clone();
-            let account = Account::Basic(BasicAccount {
-                balance: genesis_account.balance,
-            });
-            debug!("Adding genesis account: {}: {:?}", address, account);
-            genesis_accounts.push((address, account));
-        }
-
-        // state root
-        let state_root = {
-            let env = VolatileEnvironment::new(10)?;
-            let accounts = Accounts::new(env.clone());
-            let mut txn = WriteTransaction::new(&env);
-            // XXX need to clone, since init needs the actual data
-            accounts.init(&mut txn, genesis_accounts.clone());
-            accounts.hash(Some(&txn))
-        };
+        // State root
+        let state_root = accounts.get_root(Some(&txn));
         debug!("State root: {}", &state_root);
 
         // the header
@@ -263,27 +247,36 @@ impl GenesisBuilder {
         })
     }
 
-    fn generate_staking_contract(&self) -> Result<StakingContract, GenesisBuilderError> {
-        let mut contract = StakingContract::default();
+    fn generate_staking_contract(
+        &self,
+        accounts: &Accounts,
+        txn: &mut WriteTransaction,
+    ) -> Result<(), GenesisBuilderError> {
+        StakingContract::create(&accounts.tree, txn);
 
-        for validator in self.validators.iter() {
-            contract.create_validator(
-                validator.validator_address.clone(),
+        for validator in &self.validators {
+            StakingContract::create_validator(
+                &accounts.tree,
+                txn,
+                &validator.validator_address,
+                validator.warm_address.clone(),
                 validator.validator_key.compress(),
                 validator.reward_address.clone(),
-                validator.balance,
+                None,
             )?;
         }
 
-        for stake in self.stakes.iter() {
-            contract.create_staker(
-                stake.staker_address.clone(),
-                stake.balance,
-                &stake.validator_id,
+        for staker in &self.stakers {
+            StakingContract::create_staker(
+                &accounts.tree,
+                txn,
+                &staker.staker_address,
+                staker.balance,
+                Some(staker.delegation.clone()),
             )?;
         }
 
-        Ok(contract)
+        Ok(())
     }
 
     pub fn write_to_files<P: AsRef<Path>>(
