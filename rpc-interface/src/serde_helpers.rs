6c1aeea5ec947fca0aa40671127dd8b90fce8593
Sören <hello@soerenschwert.de>
2021-09-27 14:35:29 +0200

    Remove outdated & unused Serde helpers

--- rpc-interface/src/serde_helpers.rs
+++ rpc-interface/src/serde_helpers.rs
@@ -35,54 +35,6 @@ pub mod account_type {
     }
 }
 
-pub mod address_hex {
-    use serde::{
-        de::{Deserialize, Deserializer, Error},
-        ser::{Serialize, Serializer},
-    };
-
-    use nimiq_keys::Address;
-
-    pub fn serialize<S>(address: &Address, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        Serialize::serialize(&address.to_hex(), serializer)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Address, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s: &'de str = Deserialize::deserialize(deserializer)?;
-        s.parse().map_err(D::Error::custom)
-    }
-}
-
-pub mod address_friendly {
-    use serde::{
-        de::{Deserialize, Deserializer, Error},
-        ser::{Serialize, Serializer},
-    };
-
-    use nimiq_keys::Address;
-
-    pub fn serialize<S>(address: &Address, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        Serialize::serialize(&address.to_user_friendly_address(), serializer)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Address, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s: &'de str = Deserialize::deserialize(deserializer)?;
-        Address::from_user_friendly_address(s).map_err(D::Error::custom)
-    }
-}
-
 pub mod hex {
     // TODO: Make generic over `ToHex` and `FromHex`. Or use `serde_hex`
 
