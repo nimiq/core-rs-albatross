b3bf19967317792b4d2f05b313670131dde48967
brunoffranca <bruno@franca.xyz>
2021-10-07 03:02:46 +0100

    Finished Validator RPC methods. Fixed some compilation issues on rpc-client crate. The whole project builds now.

--- rpc-interface/src/validator.rs
+++ rpc-interface/src/validator.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 
+use nimiq_keys::Address;
+
 #[cfg_attr(
     feature = "proxy",
     nimiq_jsonrpc_derive::proxy(name = "ValidatorProxy", rename_all = "camelCase")
@@ -7,4 +9,10 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait ValidatorInterface {
     type Error;
+
+    async fn get_address(&mut self) -> Result<Address, Self::Error>;
+
+    async fn get_warm_key(&mut self) -> Result<String, Self::Error>;
+
+    async fn get_hot_key(&mut self) -> Result<String, Self::Error>;
 }
--- rpc-server/src/dispatchers/validator.rs
+++ rpc-server/src/dispatchers/validator.rs
@@ -1,14 +1,19 @@
 use async_trait::async_trait;
+use beserial::Serialize;
+
+use nimiq_keys::Address;
+use nimiq_rpc_interface::validator::ValidatorInterface;
+use nimiq_validator::validator::ValidatorProxy;
 
 use crate::error::Error;
 
-use nimiq_rpc_interface::validator::ValidatorInterface;
-
-pub struct ValidatorDispatcher {}
+pub struct ValidatorDispatcher {
+    validator: ValidatorProxy,
+}
 
 impl ValidatorDispatcher {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(validator: ValidatorProxy) -> Self {
+        ValidatorDispatcher { validator }
     }
 }
 
@@ -16,4 +21,24 @@ impl ValidatorDispatcher {
 #[async_trait]
 impl ValidatorInterface for ValidatorDispatcher {
     type Error = Error;
+
+    async fn get_address(&mut self) -> Result<Address, Self::Error> {
+        Ok(self.validator.validator_address.read().clone())
+    }
+
+    async fn get_warm_key(&mut self) -> Result<String, Self::Error> {
+        Ok(hex::encode(
+            self.validator.warm_key.read().private.serialize_to_vec(),
+        ))
+    }
+
+    async fn get_hot_key(&mut self) -> Result<String, Self::Error> {
+        Ok(hex::encode(
+            self.validator
+                .signing_key
+                .read()
+                .secret_key
+                .serialize_to_vec(),
+        ))
+    }
 }
