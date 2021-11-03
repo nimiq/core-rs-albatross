e834d9d93a474a34f5e3fe9e74b83d0b3e7561aa
Claudio V <claudio@nimiq.com>
2021-11-03 13:25:19 -0600

    Four validators test improvements
    Modified the script so that it can also execute deleting the db state
    Improved the script to search for blocks being not produced
    Improved script and config files so they are not hardcoded to the home
    directory

--- test-scenarios/four_validators.sh
+++ test-scenarios/four_validators.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
 max_restarts=10
+# Initializing variables
+validators=(1 2 3 4)
+pids=()
+user="$USER"
+fail=false
+foldername=$(date +%Y%m%d_%H%M%S)
+ERASE=false
+DATABASE_CLEAN=false
+
+trap cleanup_exit INT
+
+function cleanup_exit() {
+    echo "Killing all validators and finishing...."
+    for pid in ${pids[@]}; do
+        kill $pid
+    done
+    if [ "$fail" = true ] ; then
+        echo "...FAILED..."
+        exit 1
+    fi
+    exit 0
+}
 
 usage()
 {
@@ -11,12 +33,11 @@ This script launches 4 validators and restarts them
 
 OPTIONS:
    -h|--help     Show this message
-   -e|--erase    Erases the validator state as part of restarting it
+   -e|--erase    Erases all of the validator state as part of restarting it
+   -d|--db       Erases only the database state of the validator as part of restarting it 
    -r|--restarts The number of times you want to kill/restart validators (by default 10 times)
 EOF
 }
-
-ERASE=false
 
 while [ ! $# -eq 0 ]; do
     case "$1" in
@@ -32,6 +53,9 @@ while [ ! $# -eq 0 ]; do
         -e | --erase)
             ERASE=true
             ;;
+        -d | --db)
+            DATABASE_CLEAN=true
+            ;;
         -h | --help)
             usage
             exit
@@ -44,16 +68,12 @@ while [ ! $# -eq 0 ]; do
     shift
 done
 
-# Initializing variables
-validators=(1 2 3 4)
-pids=()
-user="$USER"
-fail=false
-foldername=$(date +%Y%m%d_%H%M%S)
+#Create directory for logs
 mkdir -p  temp-logs/"$foldername"
 
-# Erase all previous state
-rm -rf /home/$user/.nimiq/dev/?/*
+# Erase all previous state (if any) and start fresh
+rm -rf temp-state
+mkdir -p temp-state/dev/{1,2,3,4}
 
 #Launch the validators and store their PID
 echo "Starting validators.... "
@@ -67,6 +87,8 @@ echo "Done"
 #Let the validators produce blocks for 1 minute
 sleep 1m
 
+old_block_number=0
+
 cycles=1
 while [ $cycles -le $max_restarts ]
 do
@@ -79,9 +101,15 @@ do
     sleep 10s
 
     if [ "$ERASE" = true ] ; then
-        echo "  Erasing validator state"
-        rm -rf /home/$user/.nimiq/dev/$(($index + 1 ))/*
+        echo "  Erasing all validator state"
+        rm -rf temp-state/dev/$(($index + 1 ))/*
         echo "################################## VALIDATOR STATE DELETED ###########################  " >> temp-logs/$foldername/Validator$(($index + 1 )).txt
+    fi
+
+    if [ "$DATABASE_CLEAN" = true ] ; then
+        echo "  Erasing validator database"
+        rm -rf temp-state/dev/$(($index + 1 ))/devalbatross-history-consensus
+        echo "################################## VALIDATOR DB DELETED ###########################  " >> temp-logs/$foldername/Validator$(($index + 1 )).txt
     fi
 
     echo "################################## RESTART ###########################  " >> temp-logs/$foldername/Validator$(($index + 1 )).txt
@@ -97,6 +125,7 @@ do
     sleep_time=$((30 + $RANDOM % 200))
 
     #Produce blocks for some minutes
+    echo "  Producing blocks for $sleep_time seconds"
     sleep "$sleep_time"s
 
     #Search for panics/crashes
@@ -106,17 +135,18 @@ do
         fail=true
         break
     fi
+    #Search if blocks are being produced
+    new_block_number=$(grep -rin "Now at block #" temp-logs/$foldername/ | tail -1 | awk '{print $10}' | awk -F# '{print $2}')
+
+    if [ -z "$new_block_number" ] || [ $new_block_number -le $old_block_number ] ; then
+        echo "   !!!!   BLOCKS ARE NOT BEING PRODUCED AFTER $sleep_time seconds   !!! "
+        fail=true
+        break
+    fi
+    old_block_number=$new_block_number
+
 done
 
 sleep 30s
 
-#Kill the validators and exit
-echo "Killing all validators and finishing...."
-for pid in ${pids[@]}; do
-    kill $pid
-done
-
-if [ "$fail" = true ] ; then
-    exit 1
-fi
-exit 0
+cleanup_exit
