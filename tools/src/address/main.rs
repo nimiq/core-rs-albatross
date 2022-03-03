97f46a1f5e3e5acf4ad0ecbfd6eec8a9bd6ea036
hrxi <hrrrxi@gmail.com>
2022-03-03 01:19:28 +0100

    nimiq-address can now print information about a given key

--- tools/src/address/main.rs
+++ tools/src/address/main.rs
@@ -1,9 +1,29 @@
-extern crate nimiq_keys as keys;
+use beserial::Deserialize;
+use clap::{Arg, Command};
+use nimiq_keys::{Address, PrivateKey, PublicKey, SecureGenerate};
+use std::{error::Error, process};
 
-use keys::{Address, PrivateKey, PublicKey, SecureGenerate};
+fn parse_private_key(s: &str) -> Result<PrivateKey, Box<dyn Error>> {
+    Ok(PrivateKey::deserialize_from_vec(&hex::decode(s)?)?)
+}
 
 fn main() {
-    let private_key = PrivateKey::generate_default_csprng();
+    let matches = Command::new("nimiq-address")
+        .about("Displays address etc. of a random or specified key")
+        .arg(Arg::new("private").takes_value(true).value_name("PRIVATE"))
+        .get_matches();
+
+    let private_key = if let Some(p) = matches.value_of("private") {
+        match parse_private_key(p) {
+            Ok(p) => p,
+            Err(e) => {
+                eprintln!("Error parsing private key: {}", e);
+                process::exit(1);
+            }
+        }
+    } else {
+        PrivateKey::generate_default_csprng()
+    };
     let public_key = PublicKey::from(&private_key);
     let address = Address::from(&public_key);
 
