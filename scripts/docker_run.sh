5b86700b830791815d1758225b353bddd373585e
viquezclaudio <31874948+viquezclaudio@users.noreply.github.com>
2022-03-10 20:26:41 -0600

    Fix genesis env variable for docker environments (#655)
    The NIMIQ_OVERRIDE_DEVNET_CONFIG flag needs to be set during runtime for the different
    docker based scenarios

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -11,5 +11,5 @@ fi
 if [[ ! -e "/home/nimiq/.nimiq/client.toml" || $OVERRIDE_CONFIG_FILE -eq 1 ]]; then
     ./docker_config.sh > /home/nimiq/.nimiq/client.toml
 fi
-
+export NIMIQ_OVERRIDE_DEVNET_CONFIG=/home/nimiq/dev-albatross-4-validators.toml
 /usr/local/bin/nimiq-client $@
