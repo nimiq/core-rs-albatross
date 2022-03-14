906ed725f885f013d66787e1d6211c0cf6987dda
Jose Daniel Hernandez <jd@nimiq.com>
2022-03-14 11:53:54 -0600

    libp2p: Add request/response tests
    Add request/response tests.

---
+++ network-libp2p/tests/request_response.rs
@@ -0,0 +1,420 @@
+use std::{sync::Arc, time::Duration};
+
+use futures::StreamExt;
+use libp2p::{
+    core::multiaddr::{multiaddr, Multiaddr},
+    gossipsub::GossipsubConfigBuilder,
+    identity::Keypair,
+    swarm::KeepAlive,
+};
+use rand::{thread_rng, Rng};
+
+use beserial::{Deserialize, Serialize};
+use beserial_derive::{Deserialize, Serialize};
+use nimiq_network_interface::{
+    network::Network as NetworkInterface,
+    prelude::{Message, NetworkEvent, ResponseError, ResponseMessage},
+};
+use nimiq_network_libp2p::{
+    discovery::{
+        behaviour::DiscoveryConfig,
+        peer_contacts::{PeerContact, Protocols, Services},
+    },
+    peer::Peer,
+    Config, Network, PeerId,
+};
+use nimiq_test_log::test;
+use nimiq_utils::time::OffsetTime;
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+struct TestRequest {
+    request: u64,
+}
+impl Message for TestRequest {
+    const TYPE_ID: u64 = 42;
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+struct TestResponse {
+    response: u64,
+}
+impl Message for TestResponse {
+    const TYPE_ID: u64 = 43;
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+struct TestResponse2 {
+    response: u64,
+}
+
+impl Message for TestResponse2 {
+    const TYPE_ID: u64 = 45;
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+struct TestResponse3 {
+    response: u32,
+}
+
+impl Message for TestResponse3 {
+    // Intentionally setting another response's Type ID
+    const TYPE_ID: u64 = TestResponse::TYPE_ID;
+}
+
+#[derive(Clone, Debug)]
+struct TestNetwork {}
+
+impl TestNetwork {
+    async fn create_connected_networks() -> (Network, Network) {
+        log::debug!("creating connected test networks:");
+        let addr1 = multiaddr![Memory(thread_rng().gen::<u64>())];
+        let addr2 = multiaddr![Memory(thread_rng().gen::<u64>())];
+
+        let net1 = Network::new(Arc::new(OffsetTime::new()), network_config(addr1.clone())).await;
+        net1.listen_on(vec![addr1.clone()]).await;
+
+        let net2 = Network::new(Arc::new(OffsetTime::new()), network_config(addr2.clone())).await;
+        net2.listen_on(vec![addr2.clone()]).await;
+
+        log::debug!(address = ?addr1, peer_id = ?net1.get_local_peer_id(), "Network 1");
+        log::debug!(address = ?addr2, peer_id = ?net2.get_local_peer_id(), "Network 2");
+
+        let mut events1 = net1.subscribe_events();
+        let mut events2 = net2.subscribe_events();
+
+        log::debug!("dialing peer 1 from peer 2...");
+        net2.dial_address(addr1).await.unwrap();
+
+        log::debug!("waiting for join events");
+
+        let event1 = events1.next().await.unwrap().unwrap();
+        log::trace!(event1 = ?event1);
+        assert_peer_joined(&event1, &net2.get_local_peer_id());
+
+        let event2 = events2.next().await.unwrap().unwrap();
+        log::trace!(event2 = ?event2);
+        assert_peer_joined(&event2, &net1.get_local_peer_id());
+
+        (net1, net2)
+    }
+}
+
+fn network_config(address: Multiaddr) -> Config {
+    let keypair = Keypair::generate_ed25519();
+
+    let mut peer_contact = PeerContact {
+        addresses: vec![address],
+        public_key: keypair.public(),
+        services: Services::all(),
+        timestamp: None,
+    };
+    peer_contact.set_current_time();
+
+    let gossipsub = GossipsubConfigBuilder::default()
+        .validation_mode(libp2p::gossipsub::ValidationMode::Permissive)
+        .build()
+        .expect("Invalid Gossipsub config");
+
+    Config {
+        keypair,
+        peer_contact,
+        seeds: Vec::new(),
+        discovery: DiscoveryConfig {
+            genesis_hash: Default::default(),
+            update_interval: Duration::from_secs(60),
+            min_recv_update_interval: Duration::from_secs(30),
+            update_limit: 64,
+            protocols_filter: Protocols::all(),
+            services_filter: Services::all(),
+            min_send_update_interval: Duration::from_secs(30),
+            house_keeping_interval: Duration::from_secs(60),
+            keep_alive: KeepAlive::Yes,
+        },
+        kademlia: Default::default(),
+        gossipsub,
+        memory_transport: true,
+    }
+}
+
+fn assert_peer_joined(event: &NetworkEvent<Peer>, peer_id: &PeerId) {
+    if let NetworkEvent::PeerJoined(peer) = event {
+        assert_eq!(&peer.id, peer_id);
+    } else {
+        panic!("Event is not a NetworkEvent::PeerJoined: {:?}", event);
+    }
+}
+
+// Test that we can send a request and correctly receive the response given a proper
+// request listener is replying in the peer specified
+#[test(tokio::test)]
+async fn test_valid_request_valid_response() {
+    let (net1, net2) = TestNetwork::create_connected_networks().await;
+
+    let test_request = TestRequest { request: 42 };
+    let test_response = TestResponse { response: 43 };
+
+    // Subscribe for receiving requests
+    let mut requests = net1.receive_requests::<TestRequest>();
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    log::info!("Sending request...");
+
+    // Send the request and get future for the response
+    let response = net2
+        .request::<TestRequest, TestResponse>(test_request.clone(), net1.get_local_peer_id())
+        .await
+        .unwrap();
+
+    log::info!("Waiting for Request message...");
+    let (received_request, request_id, peer_id) = requests.next().await.unwrap();
+    log::info!(
+        "Received request {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_request
+    );
+
+    assert_eq!(received_request, test_request);
+
+    // Respond the request
+    assert!(net1
+        .respond(request_id, test_response.clone())
+        .await
+        .is_ok());
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // Check the received response
+    let (received_response, request_id, peer_id) = response.await;
+    log::info!(
+        "Received response {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_response
+    );
+
+    match received_response {
+        ResponseMessage::Response(response) => {
+            assert_eq!(response, test_response);
+        }
+        ResponseMessage::Error(e) => assert_eq!(
+            e,
+            ResponseError::DeSerializationError,
+            "Response received with error: {:?}",
+            e
+        ),
+    };
+}
+
+// Test that we can send a request and receive a `InvalidResponse` if the peer replied with
+// a message type ID that doesn't match the one we are expecting
+#[test(tokio::test)]
+async fn test_valid_request_incorrect_response_type() {
+    let (net1, net2) = TestNetwork::create_connected_networks().await;
+
+    let test_request = TestRequest { request: 42 };
+    let incorrect_response = TestResponse2 { response: 43 };
+
+    // Subscribe for receiving requests
+    let mut requests = net1.receive_requests::<TestRequest>();
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    log::info!("Sending request...");
+
+    // Send the request and get future for the response
+    let response = net2
+        .request::<TestRequest, TestResponse>(test_request.clone(), net1.get_local_peer_id())
+        .await
+        .unwrap();
+
+    log::info!("Waiting for Request message...");
+    let (received_request, request_id, peer_id) = requests.next().await.unwrap();
+    log::info!(
+        "Received request {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_request
+    );
+
+    assert_eq!(received_request, test_request);
+
+    // Respond the request
+    assert!(net1
+        .respond(request_id, incorrect_response.clone())
+        .await
+        .is_ok());
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // Check the received response
+    let (received_response, request_id, peer_id) = response.await;
+    log::info!(
+        "Received response {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_response
+    );
+
+    match received_response {
+        ResponseMessage::Response(response) => {
+            assert!(false, "Received unexpected valid response: {:?}", response);
+        }
+        ResponseMessage::Error(e) => assert_eq!(
+            e,
+            ResponseError::InvalidResponse,
+            "Response received with error: {:?}",
+            e
+        ),
+    };
+}
+
+// Test that we can send a request and receive a `DeSerializationError` if the peer replied with
+// a message with the expected type ID but refers to a completely different type in reality
+#[test(tokio::test)]
+async fn test_valid_request_incorrect_response() {
+    let (net1, net2) = TestNetwork::create_connected_networks().await;
+
+    let test_request = TestRequest { request: 42 };
+    let incorrect_response = TestResponse3 { response: 43 };
+
+    // Subscribe for receiving requests
+    let mut requests = net1.receive_requests::<TestRequest>();
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    log::info!("Sending request...");
+
+    // Send the request and get future for the response
+    let response = net2
+        .request::<TestRequest, TestResponse>(test_request.clone(), net1.get_local_peer_id())
+        .await
+        .unwrap();
+
+    log::info!("Waiting for Request message...");
+    let (received_request, request_id, peer_id) = requests.next().await.unwrap();
+    log::info!(
+        "Received request {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_request
+    );
+
+    assert_eq!(received_request, test_request);
+
+    // Respond the request
+    assert!(net1
+        .respond(request_id, incorrect_response.clone())
+        .await
+        .is_ok());
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // Check the received response
+    let (received_response, request_id, peer_id) = response.await;
+    log::info!(
+        "Received response {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_response
+    );
+
+    match received_response {
+        ResponseMessage::Response(response) => {
+            assert!(false, "Received unexpected valid response: {:?}", response);
+        }
+        ResponseMessage::Error(e) => assert_eq!(
+            e,
+            ResponseError::DeSerializationError,
+            "Response received with error: {:?}",
+            e
+        ),
+    };
+}
+
+// Test that we can send a request and receive a timeout response if no response is
+// provided.
+#[test(tokio::test)]
+async fn test_valid_request_no_response() {
+    let (net1, net2) = TestNetwork::create_connected_networks().await;
+
+    let test_request = TestRequest { request: 42 };
+
+    // Subscribe for receiving requests
+    let mut requests = net1.receive_requests::<TestRequest>();
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    log::info!("Sending request...");
+
+    // Send the request and get future for the response
+    let response = net2
+        .request::<TestRequest, TestResponse>(test_request.clone(), net1.get_local_peer_id())
+        .await
+        .unwrap();
+
+    log::info!("Waiting for Request message...");
+    let (received_request, request_id, peer_id) = requests.next().await.unwrap();
+    log::info!(
+        "Received request {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_request
+    );
+
+    assert_eq!(received_request, test_request);
+
+    // Don't respond the request: It should timeout and send the error to the request
+    // Check the received response
+    let (received_response, request_id, peer_id) = response.await;
+    log::info!(
+        "Received response {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_response
+    );
+
+    match received_response {
+        ResponseMessage::Response(response) => {
+            assert!(false, "Received unexpected valid response: {:?}", response)
+        }
+        ResponseMessage::Error(e) => assert_eq!(e, ResponseError::Timeout),
+    };
+}
+
+// Test that we can send a request and receive a timeout response if no response is sent
+// given that no receiver is registered for the requests being sent
+#[test(tokio::test)]
+async fn test_valid_request_no_response_no_receiver() {
+    let (net1, net2) = TestNetwork::create_connected_networks().await;
+
+    let test_request = TestRequest { request: 42 };
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    log::info!("Sending request...");
+
+    // Send the request and get future for the response
+    let response = net2
+        .request::<TestRequest, TestResponse>(test_request.clone(), net1.get_local_peer_id())
+        .await
+        .unwrap();
+
+    // Don't respond the request: It should timeout and send the error to the request
+    // Check the received response
+    let (received_response, request_id, peer_id) = response.await;
+    log::info!(
+        "Received response {:?} from peer {:?}: {:?}",
+        request_id,
+        peer_id,
+        received_response
+    );
+
+    match received_response {
+        ResponseMessage::Response(response) => {
+            assert!(false, "Received unexpected valid response: {:?}", response)
+        }
+        ResponseMessage::Error(e) => assert_eq!(e, ResponseError::Timeout),
+    };
+}
