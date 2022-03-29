85fd0f010fc4dd8e9c169d62c2af63ae18583f0b
hrxi <hrrrxi@gmail.com>
2022-03-29 22:51:28 +0200

    test-log: Only try the initialization routine once
    This way, the log setup only has to happen once, all other calls to the
    initialization function will simply wait for the lucky one to complete.

--- test-log/src/lib.rs
+++ test-log/src/lib.rs
@@ -1,9 +1,15 @@
+use parking_lot::Once;
+
 pub use nimiq_test_log_proc_macro::test;
+
+static INITIALIZE: Once = Once::new();
 
 #[doc(hidden)]
 pub fn initialize() {
-    let _ = tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_test_writer()
-        .try_init();
+    INITIALIZE.call_once(|| {
+        tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_test_writer()
+            .init();
+    });
 }
