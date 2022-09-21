081ca124f6d42795b6139de07442e31ccd27d70f
styppo <mail@styp-rekowsky.de>
2022-09-21 07:27:08 +0000

    Trie: introduce hybrid trie nodes that can have both children and a value

--- primitives/trie/src/error.rs
+++ primitives/trie/src/error.rs
@@ -1,7 +1,8 @@
+use beserial::SerializingError;
 use thiserror::Error;
 
 /// An enum containing possible errors that can happen in the Merkle Radix Trie.
-#[derive(Error, Debug, Clone, Eq, PartialEq)]
+#[derive(Error, Debug, Eq, PartialEq)]
 pub enum MerkleRadixTrieError {
     #[error("Prefix doesn't match node's key.")]
     WrongPrefix,
@@ -11,4 +12,14 @@ pub enum MerkleRadixTrieError {
     BranchesHaveNoValue,
     #[error("Tried to query a child that does not exist.")]
     ChildDoesNotExist,
+    #[error("Tried to store a value at the root node.")]
+    RootCantHaveValue,
+    #[error("Failed to (de)serialize a value.")]
+    SerializationFailed(SerializingError),
+}
+
+impl From<SerializingError> for MerkleRadixTrieError {
+    fn from(err: SerializingError) -> Self {
+        MerkleRadixTrieError::SerializationFailed(err)
+    }
 }
