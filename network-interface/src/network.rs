3bd679f4f6fc5f27bda0f17a1c00bb77c3d95472
Jose Daniel Hernandez <jd@nimiq.com>
2023-02-15 23:55:56 -0600

    network-interface: Improve rustdoc documentation
    Improve `rustdoc` documentation for the `network-interface` crate.

--- network-interface/src/network.rs
+++ network-interface/src/network.rs
@@ -50,66 +50,89 @@ pub trait Network: Send + Sync + Unpin + 'static {
     type PubsubId: PubsubId<Self::PeerId> + Send + Sync + Unpin;
     type RequestId: Copy + Debug + Display + Eq + Send + Sync + 'static;
 
+    /// Gets the set of connected peers
     fn get_peers(&self) -> Vec<Self::PeerId>;
+
+    /// Returns wether the current peer has a connection to another peer
     fn has_peer(&self, peer_id: Self::PeerId) -> bool;
-    /// This method returns true when the given peer provides the services flags that are required by us
+
+    /// Returns true when the given peer provides the services flags that are required by us
     fn peer_provides_required_services(&self, peer_id: Self::PeerId) -> bool;
+
+    /// Disconnects a peer with a close reason
     async fn disconnect_peer(&self, peer_id: Self::PeerId, close_reason: CloseReason);
 
+    /// Subscribes to network events
     fn subscribe_events(&self) -> SubscribeEvents<Self::PeerId>;
 
+    /// Subscribes to a Gossipsub topic
     async fn subscribe<T>(
         &self,
     ) -> Result<BoxStream<'static, (T::Item, Self::PubsubId)>, Self::Error>
     where
         T: Topic + Sync;
 
+    /// Unsubscribes from a Gossipsub topic
     async fn unsubscribe<T>(&self) -> Result<(), Self::Error>
     where
         T: Topic + Sync;
 
+    /// Publishes a message to a Gossipsub topic
     async fn publish<T>(&self, item: T::Item) -> Result<(), Self::Error>
     where
         T: Topic + Sync;
 
+    /// Validates a message received from a Gossipsub topic
     fn validate_message<T>(&self, id: Self::PubsubId, acceptance: MsgAcceptance)
     where
         T: Topic + Sync;
 
+    /// Gets a value from the distributed hash table
     async fn dht_get<K, V>(&self, k: &K) -> Result<Option<V>, Self::Error>
     where
         K: AsRef<[u8]> + Send + Sync,
         V: Deserialize + Send + Sync;
 
+    /// Puts a value to the distributed hash table
     async fn dht_put<K, V>(&self, k: &K, v: &V) -> Result<(), Self::Error>
     where
         K: AsRef<[u8]> + Send + Sync,
         V: Serialize + Send + Sync;
 
+    /// Dials a peer
     async fn dial_peer(&self, peer_id: Self::PeerId) -> Result<(), Self::Error>;
 
+    /// Dials an address
     async fn dial_address(&self, address: Self::AddressType) -> Result<(), Self::Error>;
 
+    /// Gets the local peer ID
     fn get_local_peer_id(&self) -> Self::PeerId;
 
+    /// Sends a message to a specific peer
     async fn message<M: Message>(
         &self,
         request: M,
         peer_id: Self::PeerId,
     ) -> Result<(), RequestError>;
 
+    /// Requests data from a specific peer
     async fn request<Req: Request>(
         &self,
         request: Req,
         peer_id: Self::PeerId,
     ) -> Result<Req::Response, RequestError>;
 
+    /// Receives messages from peers.
+    /// This function returns a stream where the messages are going to be propagated.
     fn receive_messages<M: Message>(&self) -> BoxStream<'static, (M, Self::PeerId)>;
 
+    /// Receives requests from peers.
+    /// This function returns a stream where the requests are going to be propagated.
     fn receive_requests<Req: Request>(
         &self,
     ) -> BoxStream<'static, (Req, Self::RequestId, Self::PeerId)>;
 
+    /// Sends a response to a specific request
     async fn respond<Req: Request>(
         &self,
         request_id: Self::RequestId,
