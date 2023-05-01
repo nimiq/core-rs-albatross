069c5263cc1d4b65122fcee889fa3b64c5044536
Jose Daniel Hernandez <jd@nimiq.com>
2023-05-01 17:28:56 -0600

    handel: Add tests for the `Level` implementation
    Add unit tests for the `Level` implementation whitin the `handel`
    crate.

--- handel/src/level.rs
+++ handel/src/level.rs
@@ -174,3 +174,123 @@ impl Level {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use rand::Rng;
+
+    use beserial::{Deserialize, Serialize};
+    use nimiq_collections::bitset::BitSet;
+    use nimiq_test_log::test;
+
+    use super::*;
+    use crate::contribution::ContributionError;
+
+    /// Dump Aggregate adding numbers.
+    #[derive(Clone, Debug, Serialize, Deserialize)]
+    pub struct Contribution {
+        contributors: BitSet,
+    }
+
+    impl AggregatableContribution for Contribution {
+        fn contributors(&self) -> BitSet {
+            self.contributors.clone()
+        }
+
+        fn combine(&mut self, _other_contribution: &Self) -> Result<(), ContributionError> {
+            unimplemented!()
+        }
+    }
+
+    #[test]
+    fn it_can_handle_empty_level() {
+        let mut rng = thread_rng();
+        let id: usize = rng.gen_range(0..10);
+        let send_expected_full_size = id + rng.gen_range(0..512);
+        let level = Level::new(id, [].to_vec(), send_expected_full_size);
+
+        // Check that the level is actually empty
+        assert!(level.is_empty());
+        assert_eq!(level.num_peers(), 0);
+
+        // Start the level
+        level.start();
+        assert!(level.state.read().send_started);
+
+        // An empty level can't be active since there is no peer to send updates to
+        assert!(!level.active());
+
+        // Check that it can properly select next peers (return empty vector)
+        assert!(level.select_next_peers(rng.gen_range(0..512)).is_empty());
+    }
+
+    #[test]
+    fn it_can_handle_non_empty_level() {
+        let mut rng = thread_rng();
+        let id: usize = rng.gen_range(0..10);
+        let num_ids = rng.gen_range(1..512);
+        let mut ids: Vec<usize> = (0..num_ids).map(|_| rng.gen_range(0..=10)).collect();
+        let send_expected_full_size = num_ids + rng.gen_range(0..512);
+        let level = Level::new(id, ids.clone(), send_expected_full_size);
+
+        // Check that the level is not empty
+        assert!(!level.is_empty());
+        assert_eq!(level.num_peers(), num_ids);
+
+        // Start the level
+        level.start();
+        assert!(level.active());
+
+        // Check that it can properly select next peers (return empty vector)
+        let select_size = rng.gen_range(1..num_ids);
+        let iterations = num_ids / select_size;
+        if id != 0 {
+            for _ in 0..iterations {
+                let exp_next_peers: Vec<usize> = ids.drain(0..select_size).collect();
+                let next_peers = level.select_next_peers(select_size);
+                log::debug!(select_size, ?exp_next_peers, ?next_peers);
+                assert_eq!(next_peers, exp_next_peers);
+            }
+        } else {
+            // For level 0 the next peers is always empty since it should be our own ID
+            assert!(level.select_next_peers(select_size).is_empty());
+        }
+    }
+
+    #[test]
+    fn it_updates_signature_to_send() {
+        let mut rng = thread_rng();
+        let id: usize = rng.gen_range(0..10);
+        let num_ids = rng.gen_range(1..512);
+        let ids: Vec<usize> = (0..num_ids).map(|_| rng.gen_range(0..=10)).collect();
+        let send_expected_full_size = num_ids + rng.gen_range(0..512);
+        let level = Level::new(id, ids.clone(), send_expected_full_size);
+
+        // Create a small contribution
+        let mut contributors = BitSet::new();
+        contributors.insert(1);
+        let contribution = Contribution { contributors };
+        assert!(!level.update_signature_to_send(&contribution)); // `state.send_signature_size` should be 1 now
+
+        // Add another contribution
+        let mut contributors = BitSet::new();
+        contributors.insert(3);
+        let contribution = Contribution { contributors };
+        assert!(!level.update_signature_to_send(&contribution)); // `state.send_signature_size` should be 2 now
+
+        // Add a smaller contribution
+        let mut contributors = BitSet::new();
+        contributors.insert(2);
+        let contribution = Contribution { contributors };
+        assert!(!level.update_signature_to_send(&contribution)); // `state.send_signature_size` should be still 2
+
+        // Now complete the `send_expected_full_size`
+        let mut contributors = BitSet::new();
+        for i in 0..send_expected_full_size {
+            contributors.insert(i);
+        }
+        let contribution = Contribution { contributors };
+        // After the next call, `state.send_signature_size` should be `num_ids` or `contributors.len()` and now it should return `true`
+        assert!(level.update_signature_to_send(&contribution));
+    }
+}
