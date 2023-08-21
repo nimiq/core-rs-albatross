3969b9b75e76fa02d66e8b1b1fb9013d8e51d91b
hrxi <hrrrxi@gmail.com>
2023-08-21 17:16:30 +0200

    Reduce direct dependencies on libp2p meta crate
    This cleans up the compilation tree a little and stops
    `network-interface` from depending on libp2p.

--- network-interface/src/lib.rs
+++ network-interface/src/lib.rs
@@ -2,4 +2,4 @@ pub mod network;
 pub mod peer_info;
 pub mod request;
 
-pub use libp2p::{core::multiaddr::multiaddr, multiaddr::Protocol, Multiaddr};
+pub use multiaddr::{multiaddr, Multiaddr, Protocol};
