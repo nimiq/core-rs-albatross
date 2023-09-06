a697b4c3b5d64c2596e2613c05a0591ddab89a6d
Jose Daniel Hernandez <jd@nimiq.com>
2023-09-06 12:41:33 -0600

    pow-migration: Add some integration tests
    Add some integration tests gated under the `pow-migration-tests`
    feature because they require an external setup: Having a PoW client
    running and passing the RPC URL using the `POW_CLIENT_URL`
    environment variable.
    The feature warrants that the tests won't get run by default and only
    when needed these tests will be run.

---
+++ pow-migration/tests/mod.rs
@@ -0,0 +1,65 @@
+//! PoW migration test is a set of test that parses data from the PoW blockchain to
+//! generate a PoS state.
+//! These tests are gated under the `pow-migration-tests` since they require external
+//! setup code.
+#[cfg(feature = "pow-migration-tests")]
+mod pow_migration_test {
+    use nimiq_database::volatile::VolatileDatabase;
+    use nimiq_genesis_builder::config::GenesisConfig;
+    use nimiq_keys::Address;
+    use nimiq_pow_migration::{migrate, BlockWindows, Error};
+    use nimiq_primitives::networks::NetworkId;
+    use nimiq_rpc::Client;
+    use nimiq_test_log::test;
+    use url::Url;
+
+    fn setup_pow_client() -> Client {
+        let url =
+            std::env::var("POW_CLIENT_URL").expect("Missing `POW_CLIENT_URL` environment variable");
+        Client::new(Url::parse(&url).expect("Could not parse provided URL"))
+    }
+
+    async fn test_pow_migration(
+        block_windows: &BlockWindows,
+        validator_address: &str,
+        network_id: NetworkId,
+    ) -> Result<GenesisConfig, Error> {
+        let env = VolatileDatabase::new(20).unwrap();
+        let client = setup_pow_client();
+        let address = Address::from_user_friendly_address(&validator_address)
+            .expect("Could not parse provided validator address");
+        migrate(&client, block_windows, env, &address, network_id).await
+    }
+
+    #[test(tokio::test)]
+    async fn window_1_testnet() {
+        let validator_address = "NQ28 GSPY V07Q DJTK Y8TG DFYD KR5Q 9KBF HV5A";
+        let block_windows = BlockWindows {
+            registration_start: 2590000,
+            registration_end: 2660000,
+            pre_stake_start: 2660000,
+            pre_stake_end: 2663100,
+            election_candidate: 2664100,
+            block_confirmations: 10,
+            readiness_window: 10,
+        };
+        let network_id = NetworkId::TestAlbatross;
+        let genesis = test_pow_migration(&block_windows, &validator_address, network_id)
+            .await
+            .unwrap();
+        assert_eq!(genesis.validators.len(), 2);
+        assert_eq!(genesis.stakers.len(), 2);
+        assert!(
+            genesis
+                .validators
+                .iter()
+                .find(
+                    |validator| validator.validator_address.to_user_friendly_address()
+                        == validator_address
+                )
+                .is_some(),
+            "Could not find expected validator ('{}') in the genesis validator set",
+            validator_address
+        )
+    }
+}
