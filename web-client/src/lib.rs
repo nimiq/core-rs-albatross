2f263fbb3aa8870278345cb6c0ba588bbc33c7c8
Sören <hello@soerenschwert.de>
2023-10-06 09:11:24 +0200

    Move unit tests into their own module

--- web-client/src/lib.rs
+++ web-client/src/lib.rs
@@ -10,58 +10,56 @@ mod transaction;
 mod utils;
 
 #[cfg(test)]
-use wasm_bindgen::JsValue;
-#[cfg(test)]
-use wasm_bindgen_test::wasm_bindgen_test;
+mod tests {
+    use wasm_bindgen::JsValue;
+    use wasm_bindgen_test::wasm_bindgen_test;
 
-#[cfg(test)]
-use crate::{
-    address::Address,
-    primitives::{
-        bls_key_pair::BLSKeyPair, key_pair::KeyPair, transaction_builder::TransactionBuilder,
-    },
-};
+    use crate::{
+        address::Address,
+        primitives::{
+            bls_key_pair::BLSKeyPair, key_pair::KeyPair, transaction_builder::TransactionBuilder,
+        },
+    };
 
-#[cfg(test)]
-#[wasm_bindgen_test]
-pub fn it_can_create_and_sign_basic_transactions() {
-    let keypair = KeyPair::generate();
+    #[wasm_bindgen_test]
+    pub fn it_can_create_and_sign_basic_transactions() {
+        let keypair = KeyPair::generate();
 
-    let mut tx = TransactionBuilder::new_basic(
-        &keypair.to_address(),
-        &Address::from_string("0000000000000000000000000000000000000000")
-            .map_err(JsValue::from)
-            .unwrap(),
-        100_00000,
-        None,
-        1,
-        5,
-    )
-    .map_err(JsValue::from)
-    .unwrap();
+        let mut tx = TransactionBuilder::new_basic(
+            &keypair.to_address(),
+            &Address::from_string("0000000000000000000000000000000000000000")
+                .map_err(JsValue::from)
+                .unwrap(),
+            100_00000,
+            None,
+            1,
+            5,
+        )
+        .map_err(JsValue::from)
+        .unwrap();
 
-    tx.sign(&keypair).map_err(JsValue::from).unwrap();
-    assert_eq!(tx.verify(None).map_err(JsValue::from), Ok(()))
-}
+        tx.sign(&keypair).map_err(JsValue::from).unwrap();
+        assert_eq!(tx.verify(None).map_err(JsValue::from), Ok(()))
+    }
 
-#[cfg(test)]
-#[wasm_bindgen_test]
-pub fn it_can_create_and_sign_validator_transactions() {
-    let keypair = KeyPair::generate();
+    #[wasm_bindgen_test]
+    pub fn it_can_create_and_sign_validator_transactions() {
+        let keypair = KeyPair::generate();
 
-    let mut tx = TransactionBuilder::new_create_validator(
-        &keypair.to_address(),
-        &keypair.to_address(),
-        &keypair.public_key(),
-        &BLSKeyPair::generate(),
-        None,
-        None,
-        1,
-        5,
-    )
-    .map_err(JsValue::from)
-    .unwrap();
+        let mut tx = TransactionBuilder::new_create_validator(
+            &keypair.to_address(),
+            &keypair.to_address(),
+            &keypair.public_key(),
+            &BLSKeyPair::generate(),
+            None,
+            None,
+            1,
+            5,
+        )
+        .map_err(JsValue::from)
+        .unwrap();
 
-    tx.sign(&keypair).map_err(JsValue::from).unwrap();
-    assert_eq!(tx.verify(None).map_err(JsValue::from), Ok(()))
+        tx.sign(&keypair).map_err(JsValue::from).unwrap();
+        assert_eq!(tx.verify(None).map_err(JsValue::from), Ok(()))
+    }
 }
