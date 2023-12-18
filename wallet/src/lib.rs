a70286b8acebb623e3ca1c44cf5cb56f4f5b65a1
Sören <hello@soerenschwert.de>
2023-12-18 16:25:41 +0100

    Move database dependency behind a feature in wallet crate
    To enable usage in WASM (database depends on mdbx, which doesn't compile to WASM).

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -1,7 +1,9 @@
 pub use multisig_account::MultiSigAccount;
 pub use wallet_account::WalletAccount;
+#[cfg(feature = "store")]
 pub use wallet_store::WalletStore;
 
 mod multisig_account;
 mod wallet_account;
+#[cfg(feature = "store")]
 mod wallet_store;
