42269113c04b44c4e5f586794d77dc69d8d09513
Jose Daniel Hernandez <jd@nimiq.com>
2024-05-09 18:14:17 -0600

    metrics-server: Update the `hyper` depdency to the latest version
    Update the `hyper` dependency to the latest version (1.3.1) for the
    `metrics-server` subcrate.

--- metrics-server/src/server.rs
+++ metrics-server/src/server.rs
@@ -1,24 +1,38 @@
-use std::{
-    net::SocketAddr,
-    sync::Arc,
-    task::{Context, Poll},
-};
+use std::{net::SocketAddr, sync::Arc};
 
+use bytes::Bytes;
 use futures::future::BoxFuture;
-use hyper::{http::StatusCode, service::Service, Body, Method, Request, Response, Server};
+use http_body_util::Full;
+use hyper::{
+    body::Incoming as IncomingBody, http::StatusCode, server::conn::http2, service::Service,
+    Method, Request, Response,
+};
+use hyper_util::rt::tokio::{TokioExecutor, TokioIo};
 use log::{error, info};
 use parking_lot::RwLock;
 use prometheus_client::{encoding::text::encode, registry::Registry};
+use tokio::net::TcpListener;
 
 pub async fn metrics_server(addr: SocketAddr, registry: Registry) -> Result<(), std::io::Error> {
-    let server = Server::bind(&addr).serve(MakeMetricService::new(registry));
-    info!("Metrics server on http://{}/metrics", server.local_addr());
-    if let Err(e) = server.await {
-        error!("server error: {}", e);
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Metrics server on http://{}/metrics", addr);
+    let metrics_service = MetricService::new(registry);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        let svc_clone = metrics_service.clone();
+        tokio::spawn(async move {
+            if let Err(error) = http2::Builder::new(TokioExecutor::new())
+                .serve_connection(io, svc_clone)
+                .await
+            {
+                error!(%error, "server error",);
+            }
+        });
     }
-    Ok(())
 }
 
+#[derive(Debug, Clone)]
 pub struct MetricService {
     reg: Arc<RwLock<Registry>>,
 }
@@ -26,10 +40,15 @@ pub struct MetricService {
 type SharedRegistry = Arc<RwLock<Registry>>;
 
 impl MetricService {
-    fn get_reg(&mut self) -> SharedRegistry {
+    pub fn new(registry: Registry) -> Self {
+        Self {
+            reg: Arc::new(RwLock::new(registry)),
+        }
+    }
+    fn get_reg(&self) -> SharedRegistry {
         Arc::clone(&self.reg)
     }
-    fn respond_with_metrics(&mut self) -> Response<Body> {
+    fn respond_with_metrics(&self) -> Response<Full<Bytes>> {
         let mut encoded = String::new();
         let reg = self.get_reg();
         encode(&mut encoded, &reg.read()).unwrap();
@@ -37,27 +56,25 @@ impl MetricService {
         Response::builder()
             .status(StatusCode::OK)
             .header(hyper::header::CONTENT_TYPE, metrics_content_type)
-            .body(Body::from(encoded))
+            .body(Full::new(Bytes::from(encoded)))
             .unwrap()
     }
-    fn respond_with_404_not_found(&mut self) -> Response<Body> {
+    fn respond_with_404_not_found(&self) -> Response<Full<Bytes>> {
         Response::builder()
             .status(StatusCode::NOT_FOUND)
-            .body(Body::from("Not found try localhost:[port]/metrics"))
+            .body(Full::new(Bytes::from(
+                "Not found try localhost:[port]/metrics",
+            )))
             .unwrap()
     }
 }
 
-impl Service<Request<Body>> for MetricService {
-    type Response = Response<Body>;
+impl Service<Request<IncomingBody>> for MetricService {
+    type Response = Response<Full<Bytes>>;
     type Error = hyper::Error;
     type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
-    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn call(&mut self, req: Request<Body>) -> Self::Future {
+    fn call(&self, req: Request<IncomingBody>) -> Self::Future {
         let req_path = req.uri().path();
         let req_method = req.method();
         let resp = if (req_method == Method::GET) && (req_path == "/metrics") {
@@ -67,33 +84,5 @@ impl Service<Request<Body>> for MetricService {
             self.respond_with_404_not_found()
         };
         Box::pin(async { Ok(resp) })
-    }
-}
-
-pub struct MakeMetricService {
-    reg: SharedRegistry,
-}
-
-impl MakeMetricService {
-    pub fn new(registry: Registry) -> MakeMetricService {
-        MakeMetricService {
-            reg: Arc::new(RwLock::new(registry)),
-        }
-    }
-}
-
-impl<T> Service<T> for MakeMetricService {
-    type Response = MetricService;
-    type Error = hyper::Error;
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
-
-    fn poll_ready(&mut self, _: &mut Context) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn call(&mut self, _: T) -> Self::Future {
-        let reg = self.reg.clone();
-        let fut = async move { Ok(MetricService { reg }) };
-        Box::pin(fut)
     }
 }
