fe27db264e2c0ddfb3b4a312f92426c4ee2642b5
Pascal Berrang <git@paberr.net>
2024-06-18 18:40:19 +0300

    Use a cursor append to improve efficiency of the MMR store.

--- database/src/mdbx/cursor.rs
+++ database/src/mdbx/cursor.rs
@@ -255,4 +255,14 @@ impl<'txn> WriteCursor<'txn> for MdbxWriteCursor<'txn> {
     fn remove(&mut self) {
         self.cursor.del(WriteFlags::empty()).unwrap();
     }
+
+    fn append<K, V>(&mut self, key: &K, value: &V)
+    where
+        K: AsDatabaseBytes + ?Sized,
+        V: AsDatabaseBytes + ?Sized,
+    {
+        let key = AsDatabaseBytes::as_database_bytes(key);
+        let value = AsDatabaseBytes::as_database_bytes(value);
+        self.cursor.put(&key, &value, WriteFlags::APPEND).unwrap();
+    }
 }
