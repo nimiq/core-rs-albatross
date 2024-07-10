00704409861caaffcf43f46d4f9e34972599b490
Sören <hello@soerenschwert.de>
2024-07-10 18:17:15 +0200

    Clean up web-client NodeJS example

--- web-client/example/node/index.js
+++ web-client/example/node/index.js
@@ -6,9 +6,14 @@ async function main() {
 
     const client = await Nimiq.Client.create(config.build());
 
+    let peerCount = 0;
+    client.addPeerChangedListener((_peerId, _reason, newPeerCount) => {
+        peerCount = newPeerCount;
+    })
+
     setInterval(async () => {
         const consensus = await client.isConsensusEstablished();
-        console.log(`Consensus ${consensus ? 'established' : 'not established'}`);
+        console.log(`Consensus ${consensus ? 'established' : 'not established'}, ${peerCount} peers`);
     }, 1000);
 }
 
--- web-client/example/node/index.mjs
+++ web-client/example/node/index.mjs
@@ -5,7 +5,12 @@ const config = new Nimiq.ClientConfiguration();
 
 const client = await Nimiq.Client.create(config.build());
 
+let peerCount = 0;
+client.addPeerChangedListener((_peerId, _reason, newPeerCount) => {
+    peerCount = newPeerCount;
+})
+
 setInterval(async () => {
     const consensus = await client.isConsensusEstablished();
-    console.log(`Consensus ${consensus ? 'established' : 'not established'}`);
+    console.log(`Consensus ${consensus ? 'established' : 'not established'}, ${peerCount} peers`);
 }, 1000);
