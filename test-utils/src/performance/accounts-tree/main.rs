bb9ea31eb33de752372bda7aacea4f64bf2b90eb
viquezclaudio <viquezclaudio@gmail.com>
2024-07-16 11:38:45 -0600

    Loops support

--- test-utils/src/performance/accounts-tree/main.rs
+++ test-utils/src/performance/accounts-tree/main.rs
@@ -15,11 +15,14 @@ use nimiq_genesis::NetworkId;
 use nimiq_genesis_builder::GenesisBuilder;
 use nimiq_keys::{Address, Ed25519PublicKey, KeyPair, SecureGenerate};
 use nimiq_primitives::policy::Policy;
+use nimiq_test_utils::test_transaction::TestAccount;
 use nimiq_test_utils::{
     test_rng::test_rng,
     test_transaction::{generate_accounts, generate_transactions, TestTransaction},
 };
 
+use rand::CryptoRng;
+use rand::Rng;
 use tempfile::tempdir;
 
 /// Command line arguments for the binary
@@ -37,6 +40,14 @@ struct Args {
     loops: Option<u32>,
 }
 
+fn init_accounts<R: Rng + CryptoRng>(
+    genesis_builder: &mut GenesisBuilder,
+    sender_balances: Vec<u64>,
+    rng: &mut R,
+) -> Vec<TestAccount> {
+    generate_accounts(sender_balances, genesis_builder, true, rng)
+}
+
 fn accounts_tree_populate(
     env: &DatabaseProxy,
     tpb: u32,
@@ -45,47 +56,22 @@ fn accounts_tree_populate(
     _db_file: PathBuf,
 ) {
     let num_txns = tpb;
-    //let loops = loops - 1;
-    let batches = batches * loops;
+
     let number_of_blocks = Policy::blocks_per_batch() * batches;
     let total_txns = number_of_blocks * num_txns;
 
     let mut txns_per_block = Vec::new();
 
     let sender_balances = vec![100_000_000_000; 1];
-    let recipient_balances = vec![0; total_txns as usize];
+
     let mut genesis_builder = GenesisBuilder::default();
     genesis_builder.with_network(NetworkId::UnitAlbatross);
     let rewards = vec![];
 
     // Generate and sign transaction from an address
     let mut rng = test_rng(true);
-    println!("Generating accounts and transactions...");
-    // Generate accounts
-    let recipient_accounts =
-        generate_accounts(recipient_balances, &mut genesis_builder, false, &mut rng);
-    let sender_accounts = generate_accounts(sender_balances, &mut genesis_builder, true, &mut rng);
 
-    let mut txn_index = 0;
-
-    for _block in 0..number_of_blocks {
-        // Generate a new set of txns for each block
-        let mut mempool_transactions = vec![];
-        for _i in 0..num_txns {
-            let mempool_transaction = TestTransaction {
-                fee: 0_u64,
-                value: 1,
-                recipient: recipient_accounts[txn_index as usize].clone(),
-                sender: sender_accounts[0].clone(),
-            };
-            mempool_transactions.push(mempool_transaction);
-            txn_index += 1;
-        }
-        let (txns, _) = generate_transactions(mempool_transactions.clone(), false);
-        txns_per_block.push(txns);
-    }
-
-    println!("Done generating {} transactions and accounts", txn_index);
+    let sender_accounts = init_accounts(&mut genesis_builder, sender_balances.clone(), &mut rng);
 
     // Add validator to genesis
     genesis_builder.with_genesis_validator(
@@ -122,47 +108,87 @@ fn accounts_tree_populate(
 
     println!("Done adding accounts to genesis");
 
-    let mut block_index = 0;
+    for loop_number in 1..loops + 1 {
+        println!("Starting loop: {}", loop_number);
+        println!("-----------------");
+        println!("Generating transactions...");
+        // Generate recipients accounts
+        let recipient_balances = vec![0; total_txns as usize];
+        let recipient_accounts =
+            generate_accounts(recipient_balances, &mut genesis_builder, false, &mut rng);
 
-    for batch in 0..batches {
-        let mut txn = env.write_transaction();
+        let mut txn_index = 0;
 
-        let batch_start = Instant::now();
-
-        for _blocks in 0..Policy::blocks_per_batch() {
-            let block_start = Instant::now();
-
-            let block_state = BlockState::new(block_index, 1);
-            let result = accounts.commit(
-                &mut (&mut txn).into(),
-                &txns_per_block[block_index as usize],
-                &rewards[..],
-                &block_state,
-                &mut BlockLogger::empty(),
-            );
-            match result {
-                Ok(_) => assert!(true),
-                Err(err) => assert!(false, "Received {}", err),
-            };
-
-            let block_duration = block_start.elapsed();
-            println!(
-                "Processed block {}, duration {}ms",
-                block_index,
-                block_duration.as_millis()
-            );
-
-            block_index += 1;
+        for _block in 0..number_of_blocks {
+            // Generate a new set of txns for each block
+            let mut mempool_transactions = vec![];
+            for _i in 0..num_txns {
+                let mempool_transaction = TestTransaction {
+                    fee: 0_u64,
+                    value: 1,
+                    recipient: recipient_accounts[txn_index as usize].clone(),
+                    sender: sender_accounts[0].clone(),
+                };
+                mempool_transactions.push(mempool_transaction);
+                txn_index += 1;
+            }
+            let (txns, _) = generate_transactions(mempool_transactions.clone(), false);
+            txns_per_block.push(txns);
         }
 
-        txn.commit();
+        println!("Done generating {} transactions", txn_index);
 
-        let batch_duration = batch_start.elapsed();
+        let mut block_index = 0;
 
+        let loop_start = Instant::now();
+
+        for batch in 0..batches {
+            let mut txn = env.write_transaction();
+
+            let batch_start = Instant::now();
+
+            for _blocks in 0..Policy::blocks_per_batch() {
+                //let block_start = Instant::now();
+
+                let block_state = BlockState::new(block_index, 1);
+                let result = accounts.commit(
+                    &mut (&mut txn).into(),
+                    &txns_per_block[block_index as usize],
+                    &rewards[..],
+                    &block_state,
+                    &mut BlockLogger::empty(),
+                );
+                match result {
+                    Ok(_) => assert!(true),
+                    Err(err) => assert!(false, "Received {}", err),
+                };
+
+                //let block_duration = block_start.elapsed();
+                //println!(
+                //    "Processed block {}, duration {}ms",
+                //    block_index,
+                //    block_duration.as_millis()
+                //);
+
+                block_index += 1;
+            }
+
+            txn.commit();
+
+            let batch_duration = batch_start.elapsed();
+
+            println!(
+                "---  Processed batch {}, duration {}ms  ---",
+                batch,
+                batch_duration.as_millis()
+            );
+        }
+
+        let loop_duration = loop_start.elapsed();
         println!(
-            "---  Processed batch {}, duration {}ms  ---",
-            batch,
-            batch_duration.as_millis()
+            "Processed loop {}, duration {:.2}s",
+            loop_number,
+            loop_duration.as_millis() as f64 / 1000_f64
         );
     }
 }
@@ -182,11 +208,7 @@ fn main() {
 
     let loops = args.loops.unwrap_or(1);
 
-    for loop_number in 1..(1 + loops) {
-        println!("Current loop {}", loop_number);
-
-        accounts_tree_populate(&env, args.tpb, args.batches, loop_number, db_file.clone());
-    }
+    accounts_tree_populate(&env, args.tpb, args.batches, loops, db_file.clone());
 
     let _ = fs::remove_dir_all(temp_dir);
 
