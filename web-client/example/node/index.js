1790b94a602de0d096ebec024c4a7432f50ecc62
Sören <hello@soerenschwert.de>
2024-08-05 13:35:09 +0200

    Add peer and block stats to Node example output

--- web-client/example/node/index.js
+++ web-client/example/node/index.js
@@ -6,9 +6,20 @@ async function main() {
 
     const client = await Nimiq.Client.create(config.build());
 
+    let peerCount = 0;
+    client.addPeerChangedListener((id, reason, count) => {
+        peerCount = count;
+    });
+
+    let blockHeight = 0;
+    client.addHeadChangedListener(async () => {
+        const block = await client.getHeadBlock();
+        blockHeight = block.height;
+    })
+
     setInterval(async () => {
         const consensus = await client.isConsensusEstablished();
-        console.log(`Consensus ${consensus ? 'established' : 'not established'}`);
+        console.log(`Consensus ${consensus ? 'established' : 'not established'} - Peers: ${peerCount}, Block height: ${blockHeight}`);
     }, 1000);
 }
 
--- web-client/example/node/index.mjs
+++ web-client/example/node/index.mjs
@@ -5,7 +5,18 @@ const config = new Nimiq.ClientConfiguration();
 
 const client = await Nimiq.Client.create(config.build());
 
+let peerCount = 0;
+client.addPeerChangedListener((id, reason, count) => {
+    peerCount = count;
+});
+
+let blockHeight = 0;
+client.addHeadChangedListener(async () => {
+    const block = await client.getHeadBlock();
+    blockHeight = block.height;
+})
+
 setInterval(async () => {
     const consensus = await client.isConsensusEstablished();
-    console.log(`Consensus ${consensus ? 'established' : 'not established'}`);
+    console.log(`Consensus ${consensus ? 'established' : 'not established'} - Peers: ${peerCount}, Block height: ${blockHeight}`);
 }, 1000);
