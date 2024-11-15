9a6de81ac32c45b694847e6c8658b58016a6a77a
viquezclaudio <viquezclaudio@gmail.com>
2024-11-15 12:00:39 -0600

    Reduced the windows

--- pow-migration/src/lib.rs
+++ pow-migration/src/lib.rs
@@ -76,11 +76,11 @@ static MAINET_BLOCK_WINDOWS: &BlockWindows = &BlockWindows {
     // Pre stake ends at November 19th @ 07:00 UTC (44*1440 + 7*60 = 63780 blocks later)
     pre_stake_end: 3450500,
     // First activation window begins at November 19th @ 07:00 UTC
-    election_candidate: 3450500,
+    election_candidate: 3450400,
     // Block confirmations that are needed in order to start the migration process after candidate.
-    block_confirmations: 10,
+    block_confirmations: 3,
     // This corresponds to ~24 hours.
-    readiness_window: 60,
+    readiness_window: 10,
 };
 
 /// This enum represents the result of the migration process in an activation window
